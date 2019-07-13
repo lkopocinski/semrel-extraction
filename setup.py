badead0f49bf5643efd1210c406a9c72424a144b
Arkadiusz Janz <arkadiusz.janz@pwr.edu.pl>
2019-07-13 09:05:08 +0200

    changing the project structure, again

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='relextr',
+    version='0.1.0',
+    author='Arkadiusz Janz',
+    author_email='arkadiusz.janz@pwr.edu.pl',
+    packages=[
+        'relextr',
+        'relextr.base',
+        'relextr.model'
+    ],
+    zip_safe=False
+)
