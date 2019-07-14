0ce993c15901882da14c388186d129cd6e941b42
A. Janz <arkadiusz.janz@pwr.edu.pl>
2019-07-14 20:27:39 +0200

    refactoring

--- relextr/base/extractor.py
+++ relextr/base/extractor.py
@@ -12,12 +12,10 @@ class Parser(object):
         self._slicer = SentenceWindow(window_size=3)
         self._extractor = NounExtractor()
 
-    def __call__(self, context):
-        pass
-
-    def _doc2ctx(self, document):
+    def __call__(self, document):
         for context in self._slicer.contextify(document):
-            yield self._extractor.extract(context)
+            for first, second in self._extractor.extract(context, attr='BRAND'):
+                yield first, second
 
 
 class ContextType(object):
@@ -57,8 +55,13 @@ class ExtractorType(object):
     def is_noun(self, token):
         return cou.get_coarse_pos(token, self._tagset) == 'noun'
 
+    def extract(self, context, attr='default'):
+        analysed = self.find_attribute_tokens(context, attr)
+        to_match = self._extract(context)
+        return product(analysed, to_match)
+
     @abc.abstractmethod
-    def extract(self, context):
+    def _extract(self, context):
         pass
 
 
@@ -67,19 +70,13 @@ class NounExtractor(ExtractorType):
     def __init__(self, name='NounExtractor'):
         super(NounExtractor, self).__init__(name)
 
-    def extract(self, context, attr='BRAND'):
-        analysed = self.find_attribute_tokens(context, attr)
+    def _extract(self, context):
         matched = set()
 
         for token in self.tokens(context):
-            if not token.has_metadata():
-                continue
-            metadata = token.get_metadata()
-            if not metadata.has_attribute(attr):
-                continue
             if self.is_noun(token):
                 matched.add(cou.get_lexeme_lemma(token))
-        return product(analysed, matched)
+        return matched
 
 
 class SentenceWindow(ContextType):
@@ -100,9 +97,3 @@ class SentenceWindow(ContextType):
                 yield window
         if len(window) < self._size:
             yield window
-
-
-e = Parser()
-d = ccl.read_ccl('./test.xml')
-for ctx in e._doc2ctx(d):
-    pass
