f6f8e6d2cbc0078d8c81ca02a26e72c086472972
A. Janz <arkadiusz.janz@pwr.edu.pl>
2019-07-15 14:25:22 +0200

    new loader for ELMo
    Former-commit-id: 2a0c347a932f893d2abcdb59a2fb22d051735746

--- relextr/utils/embutils.py
+++ relextr/utils/embutils.py
@@ -1,10 +1,22 @@
+import os
+import zipfile
 from allennlp.modules.elmo import Elmo, batch_to_ids
 
 
 class ElmoEmb(object):
 
-    def __init__(self, options_file, weights_file):
-        self._model = Elmo(options_file, weights_file, 2, dropout=0)
+    def __init__(self, model_path):
+        self._model = self._load_model(model_path)
+
+    def load_model(self, model_path):
+        zipf = zipfile.ZipFile(model_path)
+        zipf.extractall('./')
+        zipf.close()
+
+        model_dir = os.path.abspath('./')
+        options_file = os.path.join(model_dir, 'emb-options.json')
+        weights_file = os.path.join(model_dir, 'emb-weights.hdf5')
+        return Elmo(options_file, weights_file, 2, dropout=0)
 
     def embedd(self, context, idx):
         character_ids = batch_to_ids([context])
