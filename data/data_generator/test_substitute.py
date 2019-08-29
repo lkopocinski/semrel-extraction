37ee17ac04b64b448189733a18b2d4b1fb02c4af
lkopocinski <lkopocinski@gmail.com>
2019-08-29 12:33:38 +0200

    Test substitution products
    Former-commit-id: c82a5781d15b7dc4d42435c94ce04c30fbe3fac2

--- data/data_generator/test_substitute.py
+++ data/data_generator/test_substitute.py
@@ -1,18 +1,48 @@
-import pytest
-
-from .gen_negative_substituted import substitute_brand
+from .gen_negative_substituted import substitute_brand, substitute_product
 
 
 def test_substitute_brand():
     brand = 'Full brand name'
     brand_idx = 2
+    brand_ctx = ['This', 'is', 'BRAND NAME', 'and', 'it', 'is', 'awesome']
+
+    product_idx = 3
+    product_ctx = ['I', 'love', 'an', 'Excellent product', 'very', 'much']
+
+    actual_idx_brand, actual_ctx_brand, actual_idx_product, actual_ctx_product = substitute_brand(
+        brand,
+        brand_idx,
+        brand_ctx,
+        product_idx,
+        product_ctx
+    )
+
+    expected_idx_brand = brand_idx
+    expected_ctx_brand = ['This', 'is', 'Full', 'brand', 'name', 'and', 'it', 'is', 'awesome']
+    expected_idx_product = product_idx
+    expected_ctx_product = ['I', 'love', 'an', 'Excellent', 'product', 'very', 'much']
+
+    assert actual_idx_brand == expected_idx_brand
+    assert actual_ctx_brand == expected_ctx_brand
+    assert actual_idx_product == expected_idx_product
+    assert actual_ctx_product == expected_ctx_product
+
+
+def test_substitute_product():
+    brand_idx = 2
     brand_ctx = ['This', 'is', 'Full brand name', 'and', 'it', 'is', 'awesome']
 
     product = 'Excellent product'
     product_idx = 3
-    product_ctx = ['I', 'love', 'an', 'Excellent product', 'very', 'much']
+    product_ctx = ['I', 'love', 'an', 'PRODUCT', 'very', 'much']
 
-    actual_idx_brand, actual_ctx_brand, actual_idx_product, actual_ctx_product = substitute_brand(brand, brand_idx, brand_ctx, product_idx, product_ctx)
+    actual_idx_brand, actual_ctx_brand, actual_idx_product, actual_ctx_product = substitute_product(
+        product,
+        brand_idx,
+        brand_ctx,
+        product_idx,
+        product_ctx
+    )
 
     expected_idx_brand = brand_idx
     expected_ctx_brand = ['This', 'is', 'Full', 'brand', 'name', 'and', 'it', 'is', 'awesome']
