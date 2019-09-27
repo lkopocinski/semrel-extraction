69fa0c58520e717b2f879148f5e451c854a4d87c
lkopocinski <lkopocinski@gmail.com>
2019-09-27 14:37:36 +0200

    Remove duplicates while generating datasets

--- data/generate.sh
+++ data/generate.sh
@@ -18,8 +18,17 @@ mkdir -p ${RES_DIR}/negative/substituted
 for nr in ${FILES_NR[*]}
 do
     FILES_PATH=${ROOT_PATH}/inforex_export_${nr}/documents/
+
     python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/positive/${nr}.context
+    sort -u ${RES_DIR}/positive/${nr}.context > ${RES_DIR}/positive/${nr}.context.u
+
     python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/negative/${nr}.context
+    sort -u ${RES_DIR}/negative/${nr}.context > ${RES_DIR}/negative/${nr}.context.u
+
     python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" --multiword True > ${RES_DIR}/positive/multiword/${nr}.context
+    sort -u ${RES_DIR}/positive/multiword/${nr}.context > ${RES_DIR}/positive/multiword/${nr}.context.u
+
     python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${RES_DIR}/positive/multiword/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${RES_DIR}/negative/substituted/${nr}.context
+    sort -u ${RES_DIR}/negative/substituted/${nr}.context > ${RES_DIR}/negative/substituted/${nr}.context.u
+
 done
