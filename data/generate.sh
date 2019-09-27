11d48f681c7a679e518c1a4b6fd1c34632fc4c63
Łukasz Kopociński <lkopocinski@gmail.com>
2019-09-27 15:10:01 +0200

    Remove duplicates while generate
    Former-commit-id: 81c0bb92f7b535c2f9b115dd3b9da540542ef361

--- data/generate.sh
+++ data/generate.sh
@@ -14,21 +14,25 @@ mkdir -p ${RES_DIR}/negative
 mkdir -p ${RES_DIR}/positive/multiword
 mkdir -p ${RES_DIR}/negative/substituted
 
+train=./lista-train.txt
+dev=./lista-dev.txt
+test=./lista-test.txt
+
 # Generate
 for nr in ${FILES_NR[*]}
 do
     FILES_PATH=${ROOT_PATH}/inforex_export_${nr}/documents/
 
     python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/positive/${nr}.context
-    sort -u ${RES_DIR}/positive/${nr}.context > ${RES_DIR}/positive/${nr}.context.u
+    sort -u -o ${RES_DIR}/positive/${nr}.context ${RES_DIR}/positive/${nr}.context
 
     python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/negative/${nr}.context
-    sort -u ${RES_DIR}/negative/${nr}.context > ${RES_DIR}/negative/${nr}.context.u
+    sort -u -o ${RES_DIR}/negative/${nr}.context ${RES_DIR}/negative/${nr}.context
 
     python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" --multiword True > ${RES_DIR}/positive/multiword/${nr}.context
-    sort -u ${RES_DIR}/positive/multiword/${nr}.context > ${RES_DIR}/positive/multiword/${nr}.context.u
+    sort -u -o ${RES_DIR}/positive/multiword/${nr}.context ${RES_DIR}/positive/multiword/${nr}.context
 
     python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${RES_DIR}/positive/multiword/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${RES_DIR}/negative/substituted/${nr}.context
-    sort -u ${RES_DIR}/negative/substituted/${nr}.context > ${RES_DIR}/negative/substituted/${nr}.context.u
+    sort -u -o ${RES_DIR}/negative/substituted/${nr}.context  ${RES_DIR}/negative/substituted/${nr}.context.u
 
 done
