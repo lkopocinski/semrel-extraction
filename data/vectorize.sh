04ad443f513feecd1b3066d3922ce9c1592e97c6
lkopocinski <lkopocinski@gmail.com>
2019-10-01 13:39:05 +0200

    Create vectorize.sh
    Former-commit-id: 0e8db659a77ad856a5c2c063a7e04e77029c921e

--- data/vectorize.sh
+++ data/vectorize.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
-options_file = '/data2/piotrmilkowski/bilm-tf-data/e2000000/options.json'
-weights_file = '/data2/piotrmilkowski/bilm-tf-data/e2000000/weights.hdf5'
+# Params
+SCRIPTS_DIR=scripts
+OPTIONS_FILE='/data2/piotrmilkowski/bilm-tf-data/e2000000/options.json'
+WEIGHTS_FILE='/data2/piotrmilkowski/bilm-tf-data/e2000000/weights.hdf5'
+
+SOURCE_PATH=''
+RELATION_TYPE='in_relation'
+#RELATION_TYPE='no_relation'
+
+python3.6 ${SCRIPTS_DIR}/create_vectors.py -s ${SOURCE_PATH}.context -r ${RELATION_TYPE} -p ${OPTIONS_FILE} -w ${WEIGHTS_FILE}
