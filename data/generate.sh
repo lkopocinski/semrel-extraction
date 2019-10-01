e415762ce589722e6f0cf9919f79bf55766b16dc
lkopocinski <lkopocinski@gmail.com>
2019-10-01 10:17:02 +0200

    Declare dirs to save results

--- data/generate.sh
+++ data/generate.sh
@@ -3,34 +3,43 @@
 # Params
 CHANNELS='['BRAND_NAME', 'PRODUCT_NAME']'
 SCRIPTS_DIR=scripts
+SPLITS_DIR=splits
 RES_DIR=generated
 BRANDS_SAMPLE=5
 
 # Initialization
-mkdir -p ${RES_DIR}/positive
-mkdir -p ${RES_DIR}/negative
-mkdir -p ${RES_DIR}/positive/multiword
-mkdir -p ${RES_DIR}/negative/substituted
-
-train=./lista-train.txt
-dev=./lista-dev.txt
-test=./lista-test.txt
+mkdir -p ${RES_DIR}
 
 # Generate
-for nr in ${FILES_NR[*]}
+declare -a types=("train" "valid" "test")
+for dataset_type in "${types[@]}"
 do
-    FILES_PATH=${ROOT_PATH}/inforex_export_${nr}/documents/
+    # Declaration
+    OUT_DIR=${RES_DIR}/${dataset_type}
+    POSITIVE_DIR=${OUT_DIR}/positive
+    NEGATIVE_DIR=${OUT_DIR}/negative
+    MULTIWORD_DIR=${OUT_DIR}/positive/multiword
+    SUBSTITUTED_DIR=${OUT_DIR}/negative/substituted
 
-    python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/positive/${nr}.context
-    sort -u -o ${RES_DIR}/positive/${nr}.context ${RES_DIR}/positive/${nr}.context
+    mkdir -p ${MULTIWORD_DIR}
+    mkdir -p ${SUBSTITUTED_DIR}
 
-    python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/negative/${nr}.context
-    sort -u -o ${RES_DIR}/negative/${nr}.context ${RES_DIR}/negative/${nr}.context
+    DATASET_LIST_PATH=${SPLITS_DIR}/${dataset_type}
+    for files_list in ${DATASET_LIST_PATH}/*.list
+    do
+        prefix=$(cut -d. -f1 ${files_list})
+        echo ${prefix}
+#        python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${files_list} -c "${CHANNELS}" > ${POSITIVE_DIR}/${prefix}.context
+#        sort -u -o ${POSITIVE_DIR}/${nr}.context ${POSITIVE_DIR}/${nr}.context
 
-    python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" --multiword True > ${RES_DIR}/positive/multiword/${nr}.context
-    sort -u -o ${RES_DIR}/positive/multiword/${nr}.context ${RES_DIR}/positive/multiword/${nr}.context
+#        python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/negative/${nr}.context
+#        sort -u -o ${RES_DIR}/negative/${nr}.context ${RES_DIR}/negative/${nr}.context
+#
+#        python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" --multiword True > ${RES_DIR}/positive/multiword/${nr}.context
+#        sort -u -o ${RES_DIR}/positive/multiword/${nr}.context ${RES_DIR}/positive/multiword/${nr}.context
+#
+#        python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${RES_DIR}/positive/multiword/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${RES_DIR}/negative/substituted/${nr}.context
+#        sort -u -o ${RES_DIR}/negative/substituted/${nr}.context  ${RES_DIR}/negative/substituted/${nr}.context.u
 
-    python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${RES_DIR}/positive/multiword/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${RES_DIR}/negative/substituted/${nr}.context
-    sort -u -o ${RES_DIR}/negative/substituted/${nr}.context  ${RES_DIR}/negative/substituted/${nr}.context.u
-
+    done
 done
