bfa6ca9a4badaad155c7c031aeb77567c95312cf
lkopocinski <lkopocinski@gmail.com>
2019-10-01 10:31:53 +0200

    Adapt script to new convention

--- data/generate.sh
+++ data/generate.sh
@@ -7,9 +7,6 @@ SPLITS_DIR=splits
 RES_DIR=generated
 BRANDS_SAMPLE=5
 
-# Initialization
-mkdir -p ${RES_DIR}
-
 # Generate
 declare -a types=("train" "valid" "test")
 for dataset_type in "${types[@]}"
@@ -27,20 +24,19 @@ do
     DATASET_LIST_PATH=${SPLITS_DIR}/${dataset_type}
     for files_list in ${DATASET_LIST_PATH}/*.list
     do
-	echo ${files_list}
+	    echo ${files_list}
         prefix=$(echo ${files_list} | cut -d"/" -f3 | cut -d"." -f1)
 
-	python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${files_list} -c "${CHANNELS}" > ${POSITIVE_DIR}/${prefix}.context
+	    python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${files_list} -c "${CHANNELS}" > ${POSITIVE_DIR}/${prefix}.context
         sort -u -o ${POSITIVE_DIR}/${nr}.context ${POSITIVE_DIR}/${nr}.context
 
-#        python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" > ${RES_DIR}/negative/${nr}.context
-#        sort -u -o ${RES_DIR}/negative/${nr}.context ${RES_DIR}/negative/${nr}.context
-#
-#        python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${FILES_PATH} -c "${CHANNELS}" --multiword True > ${RES_DIR}/positive/multiword/${nr}.context
-#        sort -u -o ${RES_DIR}/positive/multiword/${nr}.context ${RES_DIR}/positive/multiword/${nr}.context
-#
-#        python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${RES_DIR}/positive/multiword/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${RES_DIR}/negative/substituted/${nr}.context
-#        sort -u -o ${RES_DIR}/negative/substituted/${nr}.context  ${RES_DIR}/negative/substituted/${nr}.context.u
+        python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py -d ${files_list} -c "${CHANNELS}" > ${NEGATIVE_DIR}/${prefix}.context
+        sort -u -o ${NEGATIVE_DIR}/${prefix}.context ${NEGATIVE_DIR}/${prefix}.context
 
+        python3.6 ${SCRIPTS_DIR}/generate_positive_from_corpora.py -d ${files_list} -c "${CHANNELS}" --multiword True > ${MULTIWORD_DIR}/${prefix}.context
+        sort -u -o ${MULTIWORD_DIR}/${prefix}.context ${MULTIWORD_DIR}/${prefix}.context
+
+        python3.6 ${SCRIPTS_DIR}/generate_negative_substitition.py -s ${MULTIWORD_DIR}/${nr}.context --sample_size ${BRANDS_SAMPLE} > ${SUBSTITUTED_DIR}/${prefix}.context
+        sort -u -o ${SUBSTITUTED_DIR}/${prefix}.context  ${SUBSTITUTED_DIR}/${prefix}.context
     done
 done
