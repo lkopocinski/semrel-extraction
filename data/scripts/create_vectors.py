3e59c3d2d37e0607be3d65c8b6afd3421e21dc82
lkopocinski <lkopocinski@gmail.com>
2019-10-02 14:17:45 +0200

    Divide negative substituted
    Former-commit-id: 4cdd0915a4152132f1d0f1c4e1067edb1cd679a6

--- data/scripts/create_vectors.py
+++ data/scripts/create_vectors.py
@@ -4,57 +4,14 @@ import sys
 import numpy as np
 from allennlp.modules.elmo import Elmo, batch_to_ids
 
+from data.scripts.parse_utils import Relation
+
 np.set_printoptions(threshold=sys.maxsize)
 
 try:
     import argcomplete
 except ImportError:
     argcomplete = None
-
-
-class Relation:
-
-    def __init__(self, line):
-        self.line = line
-        self._from = None
-        self._to = None
-        self._init_from_line()
-
-    def _init_from_line(self):
-        line = self.line.strip()
-        by_tab = line.split('\t')
-
-        lemma_from, lemma_to = by_tab[0].replace(' : ', ':').split(':', 1)
-        channel_from, channel_to = by_tab[1].replace(' : ', ':').split(':', 1)
-        index_from, context_from = by_tab[2].split(':', 1)
-        index_to, context_to = by_tab[3].split(':', 1)
-
-        context_from = eval(context_from)
-        context_to = eval(context_to)
-
-        index_from = int(index_from)
-        index_to = int(index_to)
-
-        self._from = self.Element(lemma_from, channel_from, index_from, context_from)
-        self._to = self.Element(lemma_to, channel_to, index_to, context_to)
-
-    @property
-    def source(self):
-        return self._from
-
-    @property
-    def dest(self):
-        return self._to
-
-    class Element:
-        def __init__(self, lemma, channel, index, context):
-            self.lemma = lemma
-            self.channel = channel
-            self.index = index
-            self.context = context
-
-        def __str__(self):
-            return f'{self.lemma}\t{self.channel}\t{self.index}:{self.context}'
 
 
 class Vector:
--- data/scripts/divide_datasets.py
+++ data/scripts/divide_datasets.py
@@ -2,6 +2,9 @@ import argparse
 import glob
 import math
 import random
+from collections import defaultdict
+
+from data.scripts.parse_utils import Relation
 
 try:
     import argcomplete
@@ -31,7 +34,7 @@ def load_file(path):
 
 def save_lines(lines, path):
     with open(path, 'w', encoding='utf-8') as out_file:
-        for line in lines:    
+        for line in lines:
             out_file.write(f'{line}\n')
 
 
@@ -41,9 +44,8 @@ def get_file_name(file_path):
     return file_name
 
 
-def select_positive(source_path, size):
-    positive_path = f'{source_path}/positive/*.context'
-    files = glob.glob(positive_path)
+def select(path, size):
+    files = glob.glob(path)
     files_number = len(files)
     sample_size = math.floor(size / (1 if files_number == 0 else files_number))
 
@@ -58,6 +60,45 @@ def select_positive(source_path, size):
         yield lines, file_name
 
 
+def select_positive(source_path, size):
+    path = f'{source_path}/positive/*.context'
+    return select(path, size)
+
+
+def select_substituted(source_path, size):
+    path = f'{source_path}/negative/substituted/*.context'
+    return select(path, size)
+
+
+def select_negative(source_path, size):
+    path = f'{source_path}/negative/*.context'
+    files = glob.glob(path)
+    files_number = len(files)
+    sample_size = math.floor(size / (1 if files_number == 0 else files_number))
+
+    for file_path in files:
+        lines = load_file(file_path)
+
+        cat_dict = defaultdict[list]
+        for line in lines:
+            relation = Relation(line)
+            if relation.source.channel == '' and relation.dest.channel == '':
+                cat_dict['plain'].append(f'{relation}')
+            elif relation.source.channel == 'BRAND_CHANNEL' and relation.dest.channel == '':
+                cat_dict['brand'].append(f'{relation}')
+            elif relation.source.channel == '' and relation.dest.channel == 'PRODUCT_CHANNEL':
+                cat_dict['product'].append(f'{relation}')
+
+        for key, lines in cat_dict.items():
+            if len(lines) > sample_size:
+                lines = random.sample(lines, sample_size)
+
+            file_name = get_file_name(file_path)
+            file_name = f'{file_name}.sampled'
+
+            yield lines, file_name
+
+
 def main(argv=None):
     args = get_args(argv)
 
@@ -69,6 +110,11 @@ def main(argv=None):
 
     for lines, file_name in select_positive(source_dir, positive_size):
         save_path = f'{output_dir}/positive/{file_name}'
+        save_lines(lines, save_path)
+
+    batch_size = math.floor(negative_size / 4)
+    for lines, file_name in select_substituted(source_dir, batch_size):
+        save_path = f'{output_dir}/negative/substituted/{file_name}'
         save_lines(lines, save_path)
 
 
