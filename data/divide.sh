e0c687202ec37a9be6352a3c3b7982d365d9b383
lkopocinski <lkopocinski@gmail.com>
2019-10-02 13:12:16 +0200

    Generate positive

--- data/divide.sh
+++ data/divide.sh
@@ -5,12 +5,13 @@ RES_DIR=generated
 TRAIN_DIR=${RES_DIR}/train
 VALID_DIR=${RES_DIR}/valid
 TEST_DIR=${RES_DIR}/test
+OUTPUT_DIR=${sampled}
 
 POSITIVE_SIZE=100
 NEGATIVE_SIZE=100
 
 
-for path in TRAIN_DIR VALID_DIR TEST_DIR
+for path in TRAIN_DIR
 do
-    python3.6 divide_datsets.py --positive_size ${POSITIVE_SIZE} --negative_size ${NEGATIVE_SIZE} --path ${path}
+    python3.6 divide_datsets.py --positive_size ${POSITIVE_SIZE} --negative_size ${NEGATIVE_SIZE} --source_dir ${path} --output_dir ${OUTPUT_DIR}
 done
