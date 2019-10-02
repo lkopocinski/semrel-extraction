3133cbd64c1381d7b1a608601dc09ef4d3fa561e
Łukasz Kopociński <lkopocinski@gmail.com>
2019-10-02 12:10:06 +0200

    Generate positive
    Former-commit-id: 8c64175ce1a843212e6a953515d63613b48b0a4e

--- data/scripts/divide_datasets.py
+++ data/scripts/divide_datasets.py
@@ -1,7 +1,7 @@
 import argparse
 import glob
 import math
-from random import random
+import random
 
 try:
     import argcomplete
@@ -11,8 +11,12 @@ except ImportError:
 
 def get_args(argv=None):
     parser = argparse.ArgumentParser()
-    parser.add_argument('--positive_size', required=True, help='How many positive examples should be selected.')
-    parser.add_argument('--path', required=True, help='Path to datasets in context format.')
+    parser.add_argument('--positive_size', required=True, type=int,
+                        help='How many positive examples should be selected.')
+    parser.add_argument('--negative_size', required=True, type=int,
+                        help='How many negative examples should be selected.')
+    parser.add_argument('--source_dir', required=True, help='Path to datasets in context format.')
+    parser.add_argument('--output_dir', required=True, help='Path to datasets in context format.')
 
     if argcomplete:
         argcomplete.autocomplete(parser)
@@ -22,7 +26,7 @@ def get_args(argv=None):
 
 def load_file(path):
     with open(path, 'r', encoding='utf-8') as in_file:
-        return [line.strip() for line in in_file]
+        return in_file.readlines()
 
 
 def save_lines(lines, path):
@@ -30,27 +34,40 @@ def save_lines(lines, path):
         out_file.writelines(lines)
 
 
-def select_positive(path, size):
-    files = glob.glob(path)
+def get_file_name(file_path):
+    file_name = file_path.split('/')[-1]
+    file_name = file_name.split('.', 1)[0]
+    return file_name
+
+
+def select_positive(source_path, size):
+    positive_path = f'{source_path}/positive/*.context'
+    files = glob.glob(positive_path)
     files_number = len(files)
-    batch_size = math.floor(size / files_number)
+    sample_size = math.floor(size / 1 if files_number == 0 else files_number)
 
     for file_path in files:
         lines = load_file(file_path)
-        lines = random.sample(lines, batch_size)
+        lines = random.sample(lines, sample_size)
 
-        file = file_path.split('.')[-2]
-        yield lines, file
+        file_name = get_file_name(file_path)
+        file_name = f'{file_name}.sampled'
+
+        yield lines, file_name
 
 
 def main(argv=None):
     args = get_args(argv)
-    path = args.path
-    positive_size = args.positive_size
 
-    positive_path = f'{path}/positive/*.context'
-    for lines, file in select_positive(positive_path, positive_size):
-        save_lines(lines, path)
+    source_dir = args.source_dir
+    output_dir = args.output_dir
+
+    positive_size = args.positive_size
+    negative_size = args.positive_size
+
+    for lines, file_name in select_positive(source_dir, positive_size):
+        save_path = f'{output_dir}/positive/{file_name}'
+        save_lines(lines, save_path)
 
 
 if __name__ == '__main__':
