78825b2dcb1fb6578732d77f4820ad4ee49043d5
Łukasz Kopociński <lkopocinski@gmail.com>
2019-10-03 15:13:12 +0200

    Create vectors
    Former-commit-id: 756fa5d93ece92e3726a219048d3745d8ccc0982

--- data/vectorize.sh
+++ data/vectorize.sh
@@ -10,9 +10,9 @@ for type in "${types[@]}"
 do
     TARGET_TMP=${TARGET_DIR}/tmp/${type}
     mkdir -p ${TARGET_TMP}
-    cat ${SOURCE_DIR}/${type}/positive/*.sample > ${TARGET_TMP}/positive.context
-    cat ${SOURCE_DIR}/${type}/negative/*.sample >> ${TARGET_TMP}/negative.context
-    cat ${SOURCE_DIR}/${type}/negative/substituted/*.sample >> ${TARGET_TMP}/negative.context
+    cat ${SOURCE_DIR}/${type}/positive/*.sampled > ${TARGET_TMP}/positive.context
+    cat ${SOURCE_DIR}/${type}/negative/*.sampled > ${TARGET_TMP}/negative.context
+    cat ${SOURCE_DIR}/${type}/negative/substituted/*.sampled > ${TARGET_TMP}/negative.context
 done
 
 OPTIONS_FILE='/data2/piotrmilkowski/bilm-tf-data/e2000000/options.json'
@@ -22,9 +22,9 @@ for type in "${types[@]}"
 do
     SRC_TMP=${TARGET_DIR}/tmp/${type}
     FILE_POSITIVE=${SRC_TMP}/positive.context
-    FILE_NEGATIVE=${SRC_TMP}/${type}/negative.context
+    FILE_NEGATIVE=${SRC_TMP}/negative.context
 
-    mkdir -p ${TARGET_DIR}${type}
-    python3.6 ${SCRIPTS_DIR}/create_vectors.py -s ${FILE_POSITIVE} -r ${RELATION_TYPE} -p "in_relation" -w ${WEIGHTS_FILE} > ${TARGET_DIR}/${type}/positive.vectors
-    python3.6 ${SCRIPTS_DIR}/create_vectors.py -s ${FILE_NEGATIVE} -r ${RELATION_TYPE} -p "no_relation" -w ${WEIGHTS_FILE} > ${TARGET_DIR}/${type}/negative.vectors
+    mkdir -p ${TARGET_DIR}/${type}
+    python3.6 ${SCRIPTS_DIR}/create_vectors.py -s ${FILE_POSITIVE} -r "in_relation" -p ${OPTIONS_FILE} -w ${WEIGHTS_FILE} > ${TARGET_DIR}/${type}/positive.vectors
+    python3.6 ${SCRIPTS_DIR}/create_vectors.py -s ${FILE_NEGATIVE} -r "no_relation" -p ${OPTIONS_FILE} -w ${WEIGHTS_FILE} > ${TARGET_DIR}/${type}/negative.vectors
 done
