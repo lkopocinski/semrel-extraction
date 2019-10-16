21f05ac6731d824c84ced886793aa3262d7ab0c1
Łukasz Kopociński <lkopocinski@gmail.com>
2019-10-16 10:59:58 +0200

    Fix pos_hist.py
    Former-commit-id: 69187ace1dd8080773d0c919ce1c388cf1acba70

--- data/scripts/stats/pos_hist.py
+++ data/scripts/stats/pos_hist.py
@@ -1,17 +1,20 @@
 import os
 from collections import defaultdict
 
+import pandas as pd
 from corpus_ccl import cclutils, token_utils, corpus_object_utils
 
 hist_dict = defaultdict(lambda: defaultdict(int))
 
-for file_ in os.listdir('./'):
+path = '../../korpusy/inforex_export_81/documents/'
+
+for file_ in os.listdir(path):
     if file_.endswith('.xml') and not file_.endswith('.rel.xml'):
-        doc = cclutils.read_ccl(file_)
+        doc = cclutils.read_ccl(path+file_)
         tokens = [(sent, token) for par in doc.paragraphs() for sent in par.sentences() for token in sent.tokens()]
         for ind, (sent, token) in enumerate(tokens):
             try:
-                if token_utils.get_annotation(sent, token, 'BRAND_NAME') <= 0:
+                if token_utils.get_annotation(sent, token, 'BRAND_NAME') > 0:
                     continue
             except KeyError:
                 continue
@@ -32,12 +35,14 @@ for file_ in os.listdir('./'):
             except TypeError:
                 continue
 
-            for pos in left:
-                hist_dict[pos]['left'] += 1
+            if pos in ['subst,nom,m3,sg', 'subst,gen,m3,sg', 'subst,loc,m3,sg', 'subst,nom,f,sg', 'brev,pun']:
+                for pos_l in left:
+                    hist_dict[pos_l]['left'] += 1
 
-            hist_dict[pos]['center'] += 1
+                hist_dict[pos]['center'] += 1
 
-            for pos in right:
-                hist_dict[pos]['right'] += 1
+                for pos_r in right:
+                    hist_dict[pos_r]['right'] += 1
 
-            print(left, pos, right)
+df = pd.DataFrame.from_dict(hist_dict, orient='index', columns=['center', 'left', 'right'])
+print(df)
