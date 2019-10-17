d859f236e6c3257696bcfc1cb1950aa9b49e932d
Łukasz Kopociński <lkopocinski@gmail.com>
2019-10-17 10:41:17 +0200

    Results with conv vectors

--- relextr/model/scripts/relnet.py
+++ relextr/model/scripts/relnet.py
@@ -37,53 +37,53 @@ class RelNet(nn.Module):
         return predicted.item()
 
 
-class SyntacticRelNet(nn.Module):
-    """
-        in_dim: konkatenacja wektorow (1024), hidden_dim: warstwa ukryta (512),
-        out_dim: liczba relacji
-    """
-    # todo: change hidden_dim
-    def __init__(self, in_dim=1400, hidden_dim=512, out_dim=2, dropout=0.5):
-        super(SyntacticRelNet, self).__init__()
-	# todo: vocab size V
-	self.emb = nn.Embedding(V, 64)
-
-	self.l1 = nn.LSTM(64, 512)
-	self.l2 = nn.LSTM(64, 512)
-
-        self.f1 = nn.Linear(in_dim, hidden_dim)
-        self.f2 = nn.Linear(hidden_dim, hidden_dim)
-        self.f3 = nn.Linear(hidden_dim, out_dim)
-        self.dropout = nn.Dropout(dropout)
-
-    # how to prepare t1 and t2
-    def forward(self, x, t1, t2):
-        """ x: a concatenation of word vectors """
-
-	e1 = self.emb(t1)
-	e2 = self.emb(t2)
-
-	# todo: extract the output from last timestep
-	l1 = self.l1(e1)
-	l2 = self.l2(e2)
-
-	# todo: concatenate
-	h1 = self.f1(torch.concatenate([x, l1, l2]))
-	h2 = self.f2(h1)
-	o = self.f3(h2)
-
-        return o
-
-    def extract_layer_weights(self, layername):
-        layer = self.__dict__['_modules'][layername]
-        return layer.weight.data.numpy()
-
-    def load(self, model_path):
-        self.load_state_dict(torch.load(model_path))
-
-    def predict(self, data):
-        output = self(torch.FloatTensor([data]))
-        _, predicted = torch.max(output, dim=1)
-        return predicted.item()
-
+#class SyntacticRelNet(nn.Module):
+#    """
+#        in_dim: konkatenacja wektorow (1024), hidden_dim: warstwa ukryta (512),
+#        out_dim: liczba relacji
+#    """
+#    # todo: change hidden_dim
+#    def __init__(self, in_dim=1400, hidden_dim=512, out_dim=2, dropout=0.5):
+#        super(SyntacticRelNet, self).__init__()
+#	# todo: vocab size V
+#	self.emb = nn.Embedding(V, 64)
+#
+#	self.l1 = nn.LSTM(64, 512)
+#	self.l2 = nn.LSTM(64, 512)
+#
+#        self.f1 = nn.Linear(in_dim, hidden_dim)
+#        self.f2 = nn.Linear(hidden_dim, hidden_dim)
+#        self.f3 = nn.Linear(hidden_dim, out_dim)
+#        self.dropout = nn.Dropout(dropout)
+#
+#    # how to prepare t1 and t2
+#    def forward(self, x, t1, t2):
+#        """ x: a concatenation of word vectors """
+#
+#	e1 = self.emb(t1)
+#	e2 = self.emb(t2)
+#
+#	# todo: extract the output from last timestep
+#	l1 = self.l1(e1)
+#	l2 = self.l2(e2)
+#
+#	# todo: concatenate
+#	h1 = self.f1(torch.concatenate([x, l1, l2]))
+#	h2 = self.f2(h1)
+#	o = self.f3(h2)
+#
+#        return o
+#
+#    def extract_layer_weights(self, layername):
+#        layer = self.__dict__['_modules'][layername]
+#        return layer.weight.data.numpy()
+#
+#    def load(self, model_path):
+#        self.load_state_dict(torch.load(model_path))
+#
+#    def predict(self, data):
+#        output = self(torch.FloatTensor([data]))
+#        _, predicted = torch.max(output, dim=1)
+#        return predicted.item()
+#
 
