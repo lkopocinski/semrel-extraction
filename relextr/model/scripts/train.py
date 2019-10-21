ddcf073f264cd5f92a7bc3725012201cb18be89a
lkopocinski <lkopocinski@gmail.com>
2019-10-21 12:41:44 +0200

    Add mlflow
    Former-commit-id: 9df6893e57ba5f450f42a2d47794088a473510c2

--- relextr/model/scripts/train.py
+++ relextr/model/scripts/train.py
@@ -11,6 +11,9 @@ from torch.optim import Adagrad
 from utils import load_batches, labels2idx, \
     Metrics, save_metrics
 
+from functools import reduce
+import mlflow
+
 try:
     import argcomplete
 except ImportError:
@@ -45,6 +48,17 @@ def main(argv=None):
     train_batches = load_batches(f'{args.dataset_dir}/train.vectors', args.batch_size)
     valid_batches = load_batches(f'{args.dataset_dir}/valid.vectors', args.batch_size)
     test_batches = load_batches(f'{args.dataset_dir}/test.vectors', args.batch_size)
+        
+    train_set_size = reduce((lambda x, y: x + len(y)), train_batches, 0)
+    valid_set_size = reduce((lambda x, y: x + len(y)), valid_batches, 0)
+    test_set_size = reduce((lambda x, y: x + len(y)), test_batches, 0)
+
+    mlflow.log_param('batch_size', args.batch_size)
+    mlflow.log_param('train_set_size', train_set_size)
+    mlflow.log_param('valid_set_size', valid_set_size)
+    mlflow.log_param('test_set_size', test_set_size)
+
+    mlflow.log_param('epochs', args.epochs)
 
     best_valid_fscore = [0.0, 0.0]
 
@@ -53,13 +67,17 @@ def main(argv=None):
 
         train_metrics = train(network, optimizer, loss_func, train_batches, device)
         print(f'Train:\n{train_metrics}')
+        mlflow.log_metric('train_fscore', value=train_metrics.fscore, step=epoch)
 
         valid_metrics = evaluate(network, valid_batches, loss_func, device)
         print(f'Valid:\n{valid_metrics}')
+        mlflow.log_metric('valid_fscore', value=valid_metrics.fscore, step=epoch)
 
         if valid_metrics.fscore[0] > best_valid_fscore[0] and valid_metrics.fscore[1] > best_valid_fscore[1]:
             best_valid_fscore = valid_metrics.fscore
             torch.save(network.state_dict(), args.model_name)
+            mlflow.pytorch.log_model(network, "torch_models")
+            # mlflow.log_artifact(f'./{args.model_name}, 'path to minio')
 
     network = RelNet(out_dim=2)
     network.load(args.model_name)
@@ -68,6 +86,12 @@ def main(argv=None):
     test_metrics = evaluate(network, test_batches, loss_func, device)
     print(f'\n\nTest: {test_metrics}')
     save_metrics(test_metrics, 'metrics.txt')
+
+    mlflow.log_metric('loss', test_metrics.loss)
+    mlflow.log_metric('accuracy', test_metrics.accuracy)
+    mlflow.log_metric('precision', test_metrics.precision)
+    mlflow.log_metric('recall', test_metrics.recall)
+    mlflow.log_metric('fscore', test_metrics.fscore)
 
 
 def train(network, optimizer, loss_func, batches, device):
