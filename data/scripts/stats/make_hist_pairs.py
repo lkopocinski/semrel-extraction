2f0f518e03b44e4c35ce86fd40f2960ce2482729
Łukasz Kopociński <lkopocinski@gmail.com>
2019-10-22 18:22:12 +0200

    Fix histogram scripts
    Former-commit-id: f35e6801cf607fa956a9d515d43d196c7dc97293

--- data/scripts/stats/make_hist_pairs.py
+++ data/scripts/stats/make_hist_pairs.py
@@ -1,19 +1,18 @@
 import pandas as pd
 
-root='../generated'
+root_path='../../generated'
 paths = ['test', 'train', 'valid']
 nrs = [81, 82, 83]
 
 for path in paths:
     to_save = []
     for nr in nrs:
-        df = pd.read_csv(f'{root}/{path}/positive/{nr}.context', sep='\t')
+        df = pd.read_csv(f'{root_path}/{path}/positive/{nr}.context', sep='\t',
+                         engine='python')
         brands = df.iloc[:,0]
         to_save.append(brands)
-    
+
     hist = pd.concat(to_save)
     hist = hist.value_counts()
-    hist.to_csv(f'{path}.pairs.hist', sep='\t')
-        
-
+    hist.to_csv(f'{path}.pairs.hist', sep='\t', header=False)
 
