a8f8a6a248c4ad9194875de048a4a9c410d9f0a6
lkopocinski <lkopocinski@gmail.com>
2019-11-04 13:57:03 +0100

    Refactor train.py script
    Move metrics to separate module
    Extract mlflow logs to methods

--- relextr/model/scripts/train.py
+++ relextr/model/scripts/train.py
@@ -2,23 +2,17 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+from functools import reduce
 
+import argcomplete
+import mlflow
 import torch
 import torch.nn as nn
+from metrics import Metrics, save_metrics
 from relnet import RelNet
 from torch.autograd import Variable
 from torch.optim import Adagrad
-from utils import load_batches, labels2idx, \
-    Metrics, save_metrics
-
-from functools import reduce
-import mlflow
-import sent2vec
-
-try:
-    import argcomplete
-except ImportError:
-    argcomplete = None
+from utils import load_batches, labels2idx
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 print(f'Runing on: {device}.')
@@ -33,41 +27,58 @@ def get_args(argv=None):
                         help="Directory with train, validation, test dataset.")
     parser.add_argument('-s', '--sent2vec', required=True, type=str, help="Sent2vec word embeddings model path.")
 
-    if argcomplete:
-        argcomplete.autocomplete(parser)
-
+    argcomplete.autocomplete(parser)
     return parser.parse_args(argv)
 
 
+def init_mlflow(uri, experiment):
+    mlflow.set_tracking_uri(uri)
+    mlflow.set_experiment(experiment)
+    print(f'-- mlflow --'
+          f'\nserver: {mlflow.get_tracking_uri()}'
+          f'\nexperiment: {experiment}')
+
+
+def get_set_size(dataset_batches):
+    return reduce((lambda x, y: x + len(y)), dataset_batches, 0)
+
+
+def is_better_fscore(fscore, best_fscore):
+    return fscore[0] > best_fscore[0] and fscore[1] > best_fscore[1]
+
+
+def log_metrics(metrics, step, prefix):
+    mlflow.log_metric(key=f'{prefix}_loss', value=metrics.loss, step=step)
+    mlflow.log_metric(f'{prefix}_accuracy', metrics.accuracy, step=step)
+    mlflow.log_metric(f'{prefix}_precision_pos', metrics.precision[1], step=step)
+    mlflow.log_metric(f'{prefix}_precision_neg', metrics.precision[0], step=step)
+    mlflow.log_metric(f'{prefix}_recall_pos', metrics.recall[1], step=step)
+    mlflow.log_metric(f'{prefix}_recall_neg', metrics.recall[0], step=step)
+    mlflow.log_metric(f'{prefix}_fscore_pos', metrics.fscore[1], step=step)
+    mlflow.log_metric(f'{prefix}_fscore_neg', metrics.fscore[0], step=step)
+
+
 def main(argv=None):
-    mlflow.set_tracking_uri("http://0.0.0.0:5000")
-    print(f'mlflow server: {mlflow.get_tracking_uri()}')
+    init_mlflow(
+        uri='http://0.0.0.0:5000',
+        experiment='no_substituted_pairs_sent2vec'
+    )
+
     args = get_args(argv)
 
-    s2v = sent2vec.Sent2vecModel()
-    s2v.load_model(args.sent2vec, inference_mode=True)
-
-
-    mlflow.set_experiment('no_substituted_pairs_sent2vec')
-
-    network = RelNet(out_dim=2)
+    network = RelNet()
     network.to(device)
     optimizer = Adagrad(network.parameters())
     loss_func = nn.CrossEntropyLoss()
 
-    train_batches = load_batches(f'{args.dataset_dir}/train.vectors', s2v, args.batch_size)
-    valid_batches = load_batches(f'{args.dataset_dir}/valid.vectors', s2v, args.batch_size)
-    test_batches = load_batches(f'{args.dataset_dir}/test.vectors', s2v, args.batch_size)
-
-    train_set_size = reduce((lambda x, y: x + len(y)), train_batches, 0)
-    valid_set_size = reduce((lambda x, y: x + len(y)), valid_batches, 0)
-    test_set_size = reduce((lambda x, y: x + len(y)), test_batches, 0)
+    train_batches = load_batches(f'{args.dataset_dir}/train.vectors', args.batch_size)
+    valid_batches = load_batches(f'{args.dataset_dir}/valid.vectors', args.batch_size)
+    test_batches = load_batches(f'{args.dataset_dir}/test.vectors', args.batch_size)
 
     mlflow.log_param('batch_size', args.batch_size)
-    mlflow.log_param('train_set_size', train_set_size)
-    mlflow.log_param('valid_set_size', valid_set_size)
-    mlflow.log_param('test_set_size', test_set_size)
-
+    mlflow.log_param('train_set_size', get_set_size(train_batches))
+    mlflow.log_param('valid_set_size', get_set_size(valid_batches))
+    mlflow.log_param('test_set_size', get_set_size(test_batches))
     mlflow.log_param('epochs', args.epochs)
 
     best_valid_fscore = [0.0, 0.0]
@@ -77,37 +88,25 @@ def main(argv=None):
 
         train_metrics = train(network, optimizer, loss_func, train_batches, device)
         print(f'Train:\n{train_metrics}')
-        mlflow.log_metric(key='train_loss', value=train_metrics.loss, step=epoch)
+        log_metrics(train_metrics, epoch, 'train')
 
         valid_metrics = evaluate(network, valid_batches, loss_func, device)
         print(f'Valid:\n{valid_metrics}')
-        mlflow.log_metric(key='valid_loss', value=valid_metrics.loss, step=epoch)
+        log_metrics(valid_metrics, epoch, 'valid')
 
-        if valid_metrics.fscore[0] > best_valid_fscore[0] and valid_metrics.fscore[1] > best_valid_fscore[1]:
+        if is_better_fscore(valid_metrics.fscore, best_valid_fscore):
             best_valid_fscore = valid_metrics.fscore
             torch.save(network.state_dict(), args.model_name)
-            # mlflow.pytorch.log_model(network, "torch_models")
-            # mlflow.log_artifact(f'./{args.model_name}, 'path to minio')
+            mlflow.log_artifact(f'../{args.model_name}', '/artifacts/models/')
 
-    network = RelNet(out_dim=2)
-    network.load(args.model_name)
-    network.to(device)
-
-    test_metrics = evaluate(network, test_batches, loss_func, device)
+    # Test
+    test_metrics = test(args.model_name, test_batches, loss_func, device)
     print(f'\n\nTest: {test_metrics}')
     save_metrics(test_metrics, 'metrics.txt')
-
-    mlflow.log_metric('loss', test_metrics.loss)
-    mlflow.log_metric('accuracy', test_metrics.accuracy)
-    mlflow.log_metric('precision_pos', test_metrics.precision[1])
-    mlflow.log_metric('precision_neg', test_metrics.precision[0])
-    mlflow.log_metric('recall_pos', test_metrics.recall[1])
-    mlflow.log_metric('recall_neg', test_metrics.recall[0])
-    mlflow.log_metric('fscore_pos', test_metrics.fscore[1])
-    mlflow.log_metric('fscore_neg', test_metrics.fscore[0])
+    log_metrics(test_metrics, 0, 'test')
 
 
-def train(network, optimizer, loss_func, batches, device):
+def train(network, optimizer, loss_function, batches, device):
     metrics = Metrics()
 
     network.train()
@@ -119,7 +118,7 @@ def train(network, optimizer, loss_func, batches, device):
         data = torch.FloatTensor([data])
 
         output = network(data.to(device)).squeeze(0)
-        loss = loss_func(output, target)
+        loss = loss_function(output, target)
 
         loss.backward()
         optimizer.step()
@@ -145,6 +144,13 @@ def evaluate(network, batches, loss_function, device):
             metrics.update(output.cpu(), target.cpu(), loss.item(), len(batches))
 
     return metrics
+
+
+def test(model_path, batches, loss_function, device):
+    network = RelNet()
+    network.load(model_path)
+    network.to(device)
+    return evaluate(network, batches, loss_function, device)
 
 
 if __name__ == "__main__":
