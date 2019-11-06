beff9ab0597b23f3a23e05d26b0bb27b8ccc3809
lkopocinski <lkopocinski@gmail.com>
2019-11-06 16:47:48 +0100

    Refactor Relation class
    Former-commit-id: a9a7717db7cbf56436315d4940390207ef5684d4

--- data/scripts/parse_utils.py
+++ data/scripts/parse_utils.py
@@ -1,46 +1,41 @@
+from typing import List
+
+
 class Relation:
 
-    def __init__(self, line):
-        self.line = line
-        self._from = None
-        self._to = None
-        self._init_from_line()
+    def __init__(self, source, dest):
+        self.source = source
+        self.dest = dest
 
-    def _init_from_line(self):
-        line = self.line.strip()
-        by_tab = line.split('\t')
+    @classmethod
+    def from_line(cls, line: str):
+        row = line.strip().split('\t')
 
-        lemma_from, lemma_to = by_tab[0].replace(' : ', ':').split(':', 1)
-        channel_from, channel_to = by_tab[1].replace(' : ', ':').split(':', 1)
-        index_from, context_from = by_tab[2].split(':', 1)
-        index_to, context_to = by_tab[3].split(':', 1)
+        lemma_source, lemma_dest = row[0].replace(' : ', ':').split(':', 1)
+        channel_source, channel_dest = row[1].replace(' : ', ':').split(':', 1)
+        index_source, context_source = row[2].split(':', 1)
+        index_dest, context_dest = row[3].split(':', 1)
 
-        context_from = eval(context_from)
-        context_to = eval(context_to)
-
-        index_from = int(index_from)
-        index_to = int(index_to)
-
-        self._from = self.Element(lemma_from, channel_from, index_from, context_from)
-        self._to = self.Element(lemma_to, channel_to, index_to, context_to)
+        source = cls.Element(lemma_source, channel_source, [int(index_source)], eval(context_source))
+        dest = cls.Element(lemma_dest, channel_dest, [int(index_dest)], eval(context_dest))
+        return cls(source, dest)
 
     def __str__(self):
-        return f'{self._from.lemma} : {self._to.lemma}\t{self._from.channel} : {self._to.channel}\t{self._from.index}:{self._from.context}\t{self._to.index}:{self._to.context}'
-
-    @property
-    def source(self):
-        return self._from
-
-    @property
-    def dest(self):
-        return self._to
+        return f'{self.source.lemma} : {self.dest.lemma}' \
+            f'\t{self.source.channel} : {self.dest.channel}' \
+            f'\t{self.source.start_idx}:{self.source.context}' \
+            f'\t{self.dest.start_idx}:{self.dest.context}'
 
     class Element:
-        def __init__(self, lemma, channel, index, context):
+        def __init__(self, lemma: str, channel: str, indices: List[int], context: List[str]):
             self.lemma = lemma
             self.channel = channel
-            self.index = index
+            self.indices = indices
             self.context = context
 
+        @property
+        def start_idx(self):
+            return self.indices[0]
+
         def __str__(self):
-            return f'{self.lemma}\t{self.channel}\t{self.index}:{self.context}'
+            return f'{self.lemma}\t{self.channel}\t{self.start_idx}:{self.context}'
