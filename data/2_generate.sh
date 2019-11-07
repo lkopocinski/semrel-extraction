4644ac71fb27d47ccb5e642d9c7ad995113ee76b
lkopocinski <lkopocinski@gmail.com>
2019-11-07 15:39:56 +0100

    Run negative generation

--- data/2_generate.sh
+++ data/2_generate.sh
@@ -11,17 +11,20 @@ SCRIPTS_DIR="./data/scripts"
 
 mkdir -p ${OUTPUT_PATH}
 
+#dvc run \
+#-d ${DATA_IN} \
+#-d ${SCRIPTS_DIR}/generate_positive_from_corpora.py \
+#-o ${OUTPUT_PATH} \
+#${SCRIPTS_DIR}/generate_positive_from_corpora.py --data-in ${DATA_IN} \
+#                                                 --output-path ${OUTPUT_PATH}
+
 dvc run \
 -d ${DATA_IN} \
--d ${SCRIPTS_DIR}/generate_positive_from_corpora.py \
+-d ${SCRIPTS_DIR}/generate_negative_from_corpora.py \
 -o ${OUTPUT_PATH} \
-${SCRIPTS_DIR}/generate_positive_from_corpora.py --data-in ${DATA_IN} \
+${SCRIPTS_DIR}/generate_negative_from_corpora.py --data-in ${DATA_IN} \
                                                  --output-path ${OUTPUT_PATH}
 
 popd
 
-#OUT_DIR=${OUTPUT_PATH}/${type}
-#NEGATIVE_DIR=${OUT_DIR}/negative
-#
-#python3.6 ${SCRIPTS_DIR}/generate_negative_from_corpora.py --list_file ${list} --channels "${CHANNELS}" > ${NEGATIVE_DIR}/${name}.context
 #sort -u -o ${NEGATIVE_DIR}/${name}.context ${NEGATIVE_DIR}/${name}.context
