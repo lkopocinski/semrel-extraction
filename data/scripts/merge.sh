473dcf4923ee06f93697a2d588217f3a43693cb4
Łukasz Kopociński <lkopocinski@gmail.com>
2019-11-12 18:14:21 +0100

    Make dataset

--- data/scripts/merge.sh
+++ data/scripts/merge.sh
@@ -1,12 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash -eux
+
+pushd "$(git rev-parse --show-toplevel)"
 
 DATA_IN=$1
 OUTPUT_PATH=$2
 
+mkdir -p ${OUTPUT_PATH}
+
 declare -a set_types=("train" "valid" "test")
 for type in "${set_types[@]}"
 do
+    pwd
     OUTPUT_FILE=${OUTPUT_PATH}/${type}.vectors
     cat ${DATA_IN}/${type}/*/* > ${OUTPUT_FILE}
     shuf -o ${OUTPUT_FILE} ${OUTPUT_FILE}
 done
+
+popd
