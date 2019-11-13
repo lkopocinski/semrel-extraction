efb21d2b2de2f0d896b154a6f1be1841b5f0da8f
Łukasz Kopociński <lkopocinski@gmail.com>
2019-11-13 18:55:05 +0100

    Train and test elmoconv
    Former-commit-id: 5de5cdade74b60ad4308ff822f27c35e08a930f4

--- relextr/model/test.sh
+++ relextr/model/test.sh
@@ -25,8 +25,8 @@ CUDA_VISIBLE_DEVICES=7,8,9,10 ${SCRIPTS_DIR}/test.py --data-in ${DATA_IN} \
                                                      --model-name 'relextr_model.pt' \
                                                      --batch-size 20 \
                                                      --tracking-uri 'http://10.17.50.132:8080' \
-                                                     --experiment-name 'sent2vec' \
-                                                     --vectorizer 'sent2vec' \
-                                                     --vectors-model ${SENT2VEC_MODEL} \
+                                                     --experiment-name 'elmoconv' \
+                                                     --vectorizer 'elmoconv' \
+                                                     --vectors-model ' ' \
 
 popd
