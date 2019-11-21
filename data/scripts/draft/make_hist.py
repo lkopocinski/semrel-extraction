24d9e0a809b0df57226ced9d266b9cc2a913f81e
Łukasz Kopociński <lkopocinski@gmail.com>
2019-11-21 09:21:29 +0100

    Stats
    Former-commit-id: 9933c9d7918eb272d777b65457b2db495f73dd1c

--- data/scripts/draft/make_hist.py
+++ data/scripts/draft/make_hist.py
@@ -1,16 +1,19 @@
 import pandas as pd
 
-root_path = '../../generated'
+root_path = '../../generations'
 paths = ['test', 'train', 'valid']
 nrs = [81, 82, 83]
 
-for path in paths:
+for nr in nrs:
     to_save = []
-    for nr in nrs:
+    for path in paths:
         df = pd.read_csv(f'{root_path}/{path}/positive/{nr}.context', sep='\t| : ', engine='python')
-        brands = df.iloc[:, 0]
+        brands = df[df.iloc[:,2] == "BRAND_NAME"].iloc[:,0]
+        to_save.append(brands)
+
+        brands = df[df.iloc[:,3] == "BRAND_NAME"].iloc[:,1]
         to_save.append(brands)
 
     hist = pd.concat(to_save)
     hist = hist.value_counts()
-    hist.to_csv(f'{path}.hist', sep='\t', header=False)
+    hist.to_csv(f'{nr}.hist', sep='\t', header=False)
