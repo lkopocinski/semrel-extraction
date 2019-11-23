670ddccaa885c8ec0fbf06f29e41fbec9f14024f
Łukasz Kopociński <lkopocinski@gmail.com>
2019-11-23 11:31:41 +0100

    Make datasets before runs

--- data/scripts/draft/filter.py
+++ data/scripts/draft/filter.py
@@ -20,24 +20,25 @@ def get_brands(file_path: Path):
     return brands
 
 
-def filter(to_filter_path: Path, brands: set):
+def filter_(to_filter_path: Path, brands: set):
     with to_filter_path.open('r', encoding='utf-8') as f:
         for line in f:
             row = line.strip().split('\t')
 
             if len(row) > 3:
-                if row[4] == 'BRAND_NAME' and row[3] in brands:
-                    print(row[3:5], row[6:8])
-                elif row[7] == 'BRAND_NAME' and row[6] in brands:
-                    print(row[3:5], row[6:8])
+                if row[4] == 'BRAND_NAME' and row[3] not in brands:
+                    print(line.strip())
+                elif row[7] == 'BRAND_NAME' and row[6] not in brands:
+                    print(line.strip())
 
 
 if __name__ == '__main__':
     root = '../../../relextr/model/dataset/'
-    train_file = Path(f'{root}/train.vectors')
-    valid_file = Path(f'{root}/valid.vectors')
-    test_file = Path(f'{root}/test.vectors')
+    train_file = Path(f'{root}/all/train.vectors')
+    valid_file = Path(f'{root}/all/valid.vectors')
+    test_file = Path(f'{root}/all/test.vectors')
 
     train_brands = get_brands(train_file)
     valid_brands = get_brands(valid_file)
     brands = train_brands.union(valid_brands)
+    filter_(test_file, brands)
