5c7ee10b015b3493b790ca95ad12c218fbb22a1e
lkopocinski <lkopocinski@gmail.com>
2019-11-25 14:24:03 +0100

    Setup docker compose for mlflow
    Former-commit-id: a539db3ab2a83bd3af24578e29311216f4e58aa7

--- mlflow/deps/init.sh
+++ mlflow/deps/init.sh
@@ -2,28 +2,24 @@
 
 set -e
 
-if [ -z $FILE_DIR ]; then
-  echo >&2 "FILE_DIR must be set"
+if [[ -z $FILE_DIR ]]; then
+  echo >&2 "FILE_DIR must be set."
   exit 1
 fi
 
-if [ -z $AWS_BUCKET ]; then
-  echo >&2 "AWS_BUCKET must be set"
+if [[ -z $AWS_BUCKET ]]; then
+  echo >&2 "AWS_BUCKET must be set."
   exit 1
 fi
 
-if [ -z $AWS_ACCESS_KEY_ID ]; then
-  echo >&2 "AWS_ACCESS_KEY_ID must be set"
-  exit 1
-fi
-
-if [ -z $AWS_SECRET_ACCESS_KEY ]; then
-  echo >&2 "AWS_SECRET_ACCESS_KEY must be set"
+CREDENTIALS_FILE=/root/.aws/credentials
+if [[ -f ${CREDENTIALS_FILE} ]]; then
+  echo >&2 "Credentials file must be provided."
   exit 1
 fi
 
 mlflow server \
-    --backend-store-uri file:/${FILE_DIR} \
+    --backend-store-uri file://${FILE_DIR} \
     --default-artifact-root s3://${AWS_BUCKET} \
     --host 0.0.0.0 \
     --port $PORT
