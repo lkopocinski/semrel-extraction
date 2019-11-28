559ee69daaca84a2ef2aa364146a896324294e04
lkopocinski <lkopocinski@gmail.com>
2019-11-28 13:39:57 +0100

    Implement on tensors

--- data/scripts/combine_vectors.py
+++ data/scripts/combine_vectors.py
@@ -42,12 +42,19 @@ def load_map(map_path):
     return vec_map
 
 
-def combine_vectors(doc_id, sent_id, token_indices, vec_map):
+def combine_vectors(tensor):
     pool = nn.MaxPool1d(3, stride=2)
+    tensor = tensor.transpose(0, 2)
+    output = pool(tensor)
+    return output
+
+
+def get_tensor(doc_id, sent_id, token_indices, vec_map):
     vectors = [vec_map[(doc_id, sent_id, idx)] for idx in token_indices]
-    vec = torch.FloatTensor(vectors).unsqueeze(0)
-    output = pool(vec)
-    return str(Vector(output.numpy()))
+    vectors = torch.FloatTensor(vectors)
+    tensor = torch.zeros(1, 5, 1024)
+    tensor[:, 0:vectors.shape[1], :] = vectors
+    return tensor
 
 
 def main(argv=None):
@@ -56,45 +63,27 @@ def main(argv=None):
     pudb.set_trace()
     elmo_map = load_map(args.elmo_map)
     # elmoconv_map = load_map(args.elmoconv_map)
-    fasttext_map = load_map(args.fasttext_map)
-    retrofit_map = load_map(args.retrofit_map)
+    # fasttext_map = load_map(args.fasttext_map)
+    # retrofit_map = load_map(args.retrofit_map)
     # sent2vec_map = load_map(args.sent2vec_map)
 
     source_path = Path(f'{args.data_in}/relations.fake.context')
+
+    rel_map = {}
     for row in file_rows(source_path):
         doc_id = row[2]
-        sent_id = row[3]
-        tokens_from = eval(row[7])
-        tokens_to = eval(row[13])
 
-        elmo_from_vec = combine_vectors(doc_id, sent_id, tokens_from, elmo_map)
-        elmo_to_vec = combine_vectors(doc_id, sent_id, tokens_to, elmo_map)
+        sent_id1 = row[3]
+        tokens1 = eval(row[7])
 
-        # elmoconv_from_vec = combine_vectors(doc_id, sent_id, tokens_from, elmoconv_map)
-        # elmoconv_to_vec = combine_vectors(doc_id, sent_id, tokens_to, elmoconv_map)
+        sent_id2 = row[9]
+        tokens2 = eval(row[13])
 
-        fasttext_from_vec = combine_vectors(doc_id, sent_id, tokens_from, fasttext_map)
-        fasttext_to_vec = combine_vectors(doc_id, sent_id, tokens_from, fasttext_map)
+        rel_key = (doc_id, (sent_id1, tokens1), (sent_id2, tokens2))
+        rel_map[rel_key] = (get_tensor(doc_id, sent_id1, tokens1, elmo_map), get_tensor(doc_id, sent_id1, tokens1, elmo_map))
 
-        retrofit_from_vec = combine_vectors(doc_id, sent_id, tokens_from, retrofit_map)
-        retrofit_to_vec = combine_vectors(doc_id, sent_id, tokens_from, retrofit_map)
-
-        # sent2vec_from_vec = combine_vectors(doc_id, sent_id, tokens_from, sent2vec_map)
-        # sent2vec_to_vec = combine_vectors(doc_id, sent_id, tokens_from, sent2vec_map)
-
-        ne_from = str([float(eval(row[6]))])
-        ne_to = str([float(eval(row[12]))])
-
-        row.extend([
-            elmo_from_vec, elmo_to_vec,
-            "[0, 0, 0]", "[0, 0, 0]",
-            fasttext_from_vec, fasttext_to_vec,
-            retrofit_from_vec, retrofit_to_vec,
-            "[0, 0, 0]", "[0, 0, 0]",
-            ne_from, ne_to
-        ])
-
-        print('\t'.join(row))
+        elmo1, elmo2 = rel_map.values()
+        output = combine_vectors(torch.cat(elmo1))
 
 
 if __name__ == '__main__':
