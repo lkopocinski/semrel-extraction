df85555ca0f7f6c1f18806ee3225f8897f654b13
Łukasz Kopociński <lkopocinski@gmail.com>
2019-12-02 19:15:48 +0100

    Make starting script
    Former-commit-id: 1efc576e49cdcc67d99a1613a12caf8bebec984c

--- data/2_vectorize.sh
+++ data/2_vectorize.sh
@@ -6,13 +6,13 @@ DATA_IN="./data/generations"
 OUTPUT_PATH="./data/vectorss"
 SCRIPTS_DIR="./data/scripts"
 
-mkdir -p ${OUTPUT_PATH}
+#${SCRIPTS_DIR}/combine_vectors.py --data-in ${DATA_IN} \
+#                                 --output-path ${OUTPUT_PATH} \
+#                                 --elmo-map "./data/maps/1xx.corpus.elmo.map" \
+#                                 --fasttext-map "./data/maps/1xx.corpus.fasttext.map" \
+#                                 --retrofit-map "./data/maps/1xx.corpus.retrofit.map"
 
-
-${SCRIPTS_DIR}/combine_vectors.py --data-in ${DATA_IN} \
-                                 --output-path ${OUTPUT_PATH} \
-                                 --elmo-map "./data/maps/1xx.corpus.elmo.map" \
-                                 --fasttext-map "./data/maps/1xx.corpus.fasttext.map" \
-                                 --retrofit-map "./data/maps/1xx.corpus.retrofit.map"
+${SCRIPTS_DIR}/vectors_map_sent2vec.py --relations-file ${DATA_IN}/relations.112.114.115.context.uniq \
+                                  --sent2vec "./data/sent2vec/kgr10.bin"
 
 popd
