24f3c82713613585f39180bf8285fca95d5aea9b
Łukasz Kopociński <lkopocinski@gmail.com>
2019-12-03 14:39:13 +0100

    Change config
    Former-commit-id: 7ae383911220a68c28659e4e60ebd74bfa4d91e4

--- relextr/model/train.sh
+++ relextr/model/train.sh
@@ -5,6 +5,6 @@ pushd "$(git rev-parse --show-toplevel)"
 SCRIPTS_DIR="./relextr/model/scripts"
 CONFIG="./relextr/model/config.yaml"
 
-CUDA_VISIBLE_DEVICES=1,2,3,4,5,6 ${SCRIPTS_DIR}/train.py --config ${CONFIG}
+CUDA_VISIBLE_DEVICES=5,6 ${SCRIPTS_DIR}/train.py --config ${CONFIG}
 
 popd
