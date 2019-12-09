7a90e64a6e4bde34c27a8c7d35b7bfe8aedc07ee
lkopocinski <lkopocinski@gmail.com>
2019-12-09 11:25:46 +0100

    Combine vectors from maps

--- data/scripts/combine_vectors.py
+++ data/scripts/combine_vectors.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env python3
 
 import argparse
+import csv
 from pathlib import Path
 
 import argcomplete
 import torch
 import torch.nn as nn
 
+from io import save_lines, save_tensor
+
 
 def get_args(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-in', required=True, help='File with relations.')
     parser.add_argument('--output-path', required=True, help='Directory to save vectors.')
-    parser.add_argument('--elmo-map', required=True, nargs=2, metavar='elmo.map.pt elmo.map.keys', help="Elmo vectors and keys files.")
-    parser.add_argument('--fasttext-map', required=True, nargs=2, metavar='fasttext.map.pt fasttext.map.keys', help="Fasttext vectors and keys files.")
-    parser.add_argument('--retrofit-map', required=True, nargs=2, metavar='retrofit.map.pt retrofit.map.keys', help="Retrofit vectors and keys files.")
+    parser.add_argument('--elmo-map', required=True, nargs=2, metavar='elmo.map.pt elmo.map.keys',
+                        help="Elmo vectors and keys files.")
+    parser.add_argument('--fasttext-map', required=True, nargs=2, metavar='fasttext.map.pt fasttext.map.keys',
+                        help="Fasttext vectors and keys files.")
+    parser.add_argument('--retrofit-map', required=True, nargs=2, metavar='retrofit.map.pt retrofit.map.keys',
+                        help="Retrofit vectors and keys files.")
 
     argcomplete.autocomplete(parser)
 
@@ -27,10 +33,16 @@ def file_rows(path: Path):
             yield line.strip().split('\t')
 
 
-def load_map(vectors_file, keys_file):
-    with open(vectors_file, 'r', encoding='utf-8') as f:
-        for line in f
+def load_keys(keys_file):
+    with open(keys_file, 'r', newline='', encoding='utf-8') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter='\t')
+        return {key: idx for idx, key in enumerate(csvreader)}
 
+
+def load_map(vectors_file, keys_file):
+    keys = load_keys(keys_file)
+    vectors = torch.load(vectors_file)
+    return keys, vectors
 
 
 def max_pool(tensor):
@@ -40,69 +52,52 @@ def max_pool(tensor):
     return output.transpose(2, 1).squeeze()
 
 
-def get_tensor(doc_id, sent_id, token_indices, vec_map, vec_size=1024):
-    vectors = [vec_map[(doc_id, sent_id, idx)] for idx in token_indices]
-    vectors = torch.FloatTensor(vectors).unsqueeze(0)
-    tensor = torch.zeros(1, 5, vec_size)
-    tensor[:, 0:vectors.shape[1], :] = vectors
+def get_tensor(id_domain, id_doc, id_sent, token_indices, keys, vectors_map):
+    vectors_indices = [keys[(id_domain, id_doc, id_sent, idx)] for idx in token_indices]
+    tensor = torch.zeros(1, 5, vectors_map.shape[-1])
+    vectors = vectors_map[vectors_indices]
+    tensor[:, 0:vectors_map.shape[1], :] = vectors
     return tensor
 
 
-def make_tensors_map(path: Path, vec_map, vec_size):
-    rel_map = {}
-    for row in file_rows(path):
-        cat_id = row[0]
-        label = row[1]
-        doc_id = row[2]
+def make_relations_tensors(path: Path, keys, vectors):
+    rel_keys = []
+    rel_vectors = []
+    for id_domain, label, id_doc, id_sent1, lemma1, channel1, _, tokens1, _, id_sent2, lemma2, channel2, _, tokens2 in file_rows(
+            path):
 
-        channel1 = row[5]
-        channel2 = row[11]
-
-        sent_id1 = row[3]
-        sent_id2 = row[9]
-
-        lemma1 = row[4]
-        lemma2 = row[10]
-
-        tokens1 = eval(row[7])
-        tokens2 = eval(row[13])
-
-        if len(tokens1) > 5 or len(tokens2) > 5:
+        if len(eval(tokens1)) > 5 or len(eval(tokens2)) > 5:
             continue
 
-        rel_key = (cat_id, label, doc_id, sent_id1, sent_id2, channel1, channel2, row[7], row[13], lemma1, lemma2)
-        rel_map[rel_key] = (
-            get_tensor(doc_id, sent_id1, tokens1, vec_map, vec_size),
-            get_tensor(doc_id, sent_id2, tokens2, vec_map, vec_size)
-        )
+        rel_keys.append((
+            id_domain, label, id_doc, id_sent1, id_sent2, channel1, channel2, tokens1, tokens2, lemma1, lemma2
+        ))
+        rel_vectors.append((
+            get_tensor(id_domain, id_doc, id_sent1, tokens1, keys, vectors),
+            get_tensor(id_domain, id_doc, id_sent2, tokens2, keys, vectors)
+        ))
 
-    return rel_map
+    return rel_keys, rel_vectors
 
 
 def main(argv=None):
     args = get_args(argv)
-    elmo_map = load_map(*args.elmo_map)
-    fasttext_map = load_map(*args.fasttext_map)
-    retrofit_map = load_map(*args.retrofit_map)
+    keys_elmo, vectors_elmo = load_map(*args.elmo_map)
+    keys_fasttext, vectors_fasttext = load_map(*args.fasttext_map)
+    keys_retrofit, vectors_retrofit = load_map(*args.retrofit_map)
 
+    relations_path = Path(args.data_in)
 
+    keys, vectors = make_relations_tensors(relations_path, keys_elmo, vectors_elmo)
+    vec1, vec2 = zip(*vectors)
+    vec1, vec2 = torch.cat(vec1), torch.cat(vec2)
+    pooled1, pooled2 = max_pool(vec1), max_pool(vec2)
 
-    source_path = Path(f'{args.data_in}/relations.112.114.115.context.uniq')
-    for vec_map, vec_size, save_name in [(elmo_map, 1024, 'elmo.rel.pt'), (fasttext_map, 300, 'fasttext.rel.pt'), (retrofit_map, 300, 'retrofit.rel.pt')]:
-        rel_map = make_tensors_map(source_path, vec_map, vec_size)
-        keys, vec = zip(*rel_map.items())
-        vec1, vec2 = zip(*vec)
-        output1 = max_pool(torch.cat(vec1))
-        output2 = max_pool(torch.cat(vec2))
+    concat_dump = torch.cat([pooled1, pooled2], dim=1)
 
-        concat_dump = torch.cat([output1, output2], dim=1)
-        torch.save(concat_dump, save_name)
-
-        with open(f'{save_name}.keys', 'w', encoding='utf-8') as f:
-            for key in keys:
-                f.write(f'{key}\n')
+    save_lines(Path(f'{args.output_path}/elmo.rel.keys'), keys)
+    save_tensor(Path(f'{args.output_path}/elmo.rel.pt'), concat_dump)
 
 
 if __name__ == '__main__':
     main()
-
