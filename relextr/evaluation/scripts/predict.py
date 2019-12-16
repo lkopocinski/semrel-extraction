c72651c6f6371739145bde5426d9736fd4f46321
lkopocinski <lkopocinski@gmail.com>
2019-12-16 13:16:56 +0100

    Refactor predict
    Former-commit-id: baad0f0ac89cae331bf630d5fb29cbdb3d048e94

--- relextr/evaluation/scripts/predict.py
+++ relextr/evaluation/scripts/predict.py
@@ -4,20 +4,20 @@ import click
 import torch
 from corpus_ccl import cclutils
 
-from data.scripts.utils.vectorizers import ElmoVectorizer, FastTextVectorizer
-from relextr.model.scripts.relnet import RelNet
 from relextr.evaluation.scripts.extractor import Parser
+from relextr.model.scripts.relnet import RelNet
 
 
 class Predictor(object):
 
     def __init__(self, net_model, elmo, fasttext):
-        self._net_model = net_model
+        self._net = net_model
         self._elmo = elmo
         self._fasttext = fasttext
+        self.device = 'cpu'
 
-    def predict(self, data, device):
-        (idx1, ctx1), (idx2, ctx2) = data
+    def _make_vectors(self, pair):
+        (idx1, ctx1), (idx2, ctx2) = pair
 
         ev1 = self._elmo.embed(ctx1)[idx1]
         ev2 = self._elmo.embed(ctx2)[idx2]
@@ -26,21 +26,19 @@ class Predictor(object):
         fv2 = self._fasttext.embed(ctx2)[idx2]
 
         v = torch.cat([ev1, ev2, fv1, fv2])
-        v = v.to(device)
-        print(v.is_cuda)
+        return v.to(self.device)
 
-        return self._net_model.predict(v)
+    def _predict(self, vectors):
+        with torch.no_grad():
+            prediction = self._net(vectors)
+            prediction = torch.argmax(prediction)
+            return prediction.item()
 
-    def predict_test(self, data, device):
-        v = torch.FloatTensor(2648)
-        v = v.to(device)
-        return self._net_model.predict(v)
-
-
-def documents(fileindex):
-    with open(fileindex, 'r', encoding='utf-8') as f:
-          paths = [line.strip() for line in f if os.path.exists(line.strip())]
-    return (cclutils.read_ccl(path) for path in paths)
+    def predict(self, pair):
+        # vectors = self._make_vectors(pair)
+        vectors = torch.FloatTensor(2648)
+        predictions = self._predict(vectors)
+        return predictions
 
 
 def get_device():
@@ -49,37 +47,63 @@ def get_device():
     return device
 
 
+def load_model(model_path, vector_size=2648):
+    net = RelNet(in_dim=vector_size)
+    net.load(model_path)
+    net.eval()
+    return net
+
+
+def documents(fileindex):
+    with open(fileindex, 'r', encoding='utf-8') as f:
+        paths = [line.strip() for line in f if os.path.exists(line.strip())]
+    return (cclutils.read_ccl(path) for path in paths)
+
+
 @click.command()
-@click.option('--net_model', required=True, type=str, help="A neural model for BRAND - PRODUCT recognition")
-@click.option('--elmo_model', required=True, type=(str, str), help="A path to elmo model options, weight")
-@click.option('--fasttext_model', required=True, type=str, help="A path to fasttext model")
-@click.option('--fileindex', required=True, type=str, help="A path to the list of CCL files to process")
+@click.option(
+    '--net_model',
+    required=True,
+    type=str,
+    help="A neural model for BRAND - PRODUCT recognition"
+)
+@click.option(
+    '--elmo_model',
+    required=True,
+    type=(str, str),
+    help="A path to elmo model options, weight"
+)
+@click.option(
+    '--fasttext_model',
+    required=True,
+    type=str,
+    help="A path to fasttext model"
+)
+@click.option(
+    '--fileindex',
+    required=True,
+    type=str,
+    help="A path to the list of CCL files to process"
+)
 def main(net_model, elmo_model, fasttext_model, fileindex):
     device = get_device()
-    net = RelNet(in_dim=2648)
-    net.load(net_model)
+    net = load_model(net_model)
     net.to(device)
-    net.eval()
-
-#    elmo = ElmoVectorizer(*elmo_model)
-#    fasttext = FastTextVectorizer(fasttext_model)
+    #    elmo = ElmoVectorizer(*elmo_model)
+    #    fasttext = FastTextVectorizer(fasttext_model)
 
     elmo = None
     fasttext = None
 
     predictor = Predictor(net, elmo, fasttext)
+    predictor.device = device
     parser = Parser()
 
     for doc in documents(fileindex):
-        ccl_path = doc.path().split(';')[0]
-        out_path = f'{ccl_path}.txt'
-
-        with open(out_path, 'w', encoding='utf-8') as f:
-            for sample in parser(doc):
-                with torch.no_grad():
-                    decision = predictor.predict_test(sample, device)
-                (f_idx, f_ctx), (s_idx, s_ctx) = sample
-                f.write(f'{f_ctx[f_idx]}\t{s_ctx[s_idx]}: {decision}\n')
+        for pair in parser(doc):
+            decision = predictor.predict(pair)
+            (f_idx, f_ctx), (s_idx, s_ctx) = pair
+            print(f'{f_ctx[f_idx]}\t{s_ctx[s_idx]}: {decision}\n')
 
 
 if __name__ == '__main__':
