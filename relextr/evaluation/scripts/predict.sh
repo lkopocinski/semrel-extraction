c4f9ee8de26de281526cf164af02a2914019f963
Łukasz Kopociński <lkopocinski@gmail.com>
2019-12-17 11:31:25 +0100

    Predict docker file

---
+++ relextr/evaluation/scripts/predict.sh
@@ -0,0 +1,17 @@
+#!/bin/bash -eux
+
+pushd "$(git rev-parse --show-toplevel)"
+
+FILEINDEX_PATH=$1
+MODEL_PATH="./all.pt"
+SCRIPTS_DIR="./relextr/evaluation/scripts"
+
+mkdir -p ${OUTPUT_PATH}
+
+CUDA_VISIBLE_DEVICES=0,1 ${SCRIPTS_DIR}/predict.py \
+  --net_model ${MODEL_PATH} \
+  --elmo_model "data/elmo/emb-options.json" "data/elmo/emb-weights.hdf5" \
+  --fasttext_model "data/fasttext/kgr10.plain.skipgram.dim300.neg10.bin" \
+  --fileindex ${FILEINDEX_PATH}
+
+popd
