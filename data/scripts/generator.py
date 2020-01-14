581af4a168b615a5f7c234db632ba8aab736adc4
lkopocinski <lkopocinski@gmail.com>
2020-01-14 10:33:30 +0100

    Rename variables in negative generator

--- data/scripts/generator.py
+++ data/scripts/generator.py
@@ -2,7 +2,7 @@
 
 from itertools import permutations, product
 from pathlib import Path
-from typing import List, Iterator
+from typing import Iterator
 
 from data.scripts.models import Relation
 from data.scripts.utils.corpus import id_to_sent_dict, is_ner_relation, \
@@ -13,12 +13,11 @@ from data.scripts.utils.corpus import id_to_sent_dict, is_ner_relation, \
 
 def generate(relation_files: Iterator[Path],
              channels: tuple = ('BRAND_NAME', 'PRODUCT_NAME')):
-
     for document in relations_documents_gen(relation_files):
         sentences = id_to_sent_dict(document)
 
-        yield generate_positive(document, sentences, channels)
-        # yield generate_negative(document, sentences, channels)
+        # yield generate_positive(document, sentences, channels)
+        yield generate_negative(document, sentences, channels)
 
 
 def generate_positive(document, sentences, channels):
@@ -43,71 +42,83 @@ def generate_negative(document, sentences, channels):
     relidxs = {}
     for relation in document.relations():
         if is_ner_relation(relation) and is_in_channel(relation, channels):
-            f = relation.rel_from()
-            t = relation.rel_to()
-            f_sent_id = f.sentence_id()
-            t_sent_id = t.sentence_id()
+            sent_id_from = relation.rel_from().sentence_id()
+            sent_id_to = relation.rel_to().sentence_id()
 
-            f_element = get_relation_element(f, sentences)
-            t_element = get_relation_element(t, sentences)
-            f_indices = tuple(f_element.indices)
-            t_indices = tuple(t_element.indices)
+            element_from = get_relation_element(relation.rel_from(), sentences)
+            element_to = get_relation_element(relation.rel_to(), sentences)
 
-            relations[((f_sent_id, f_indices), (t_sent_id, t_indices))] = (
-                relation, f_element.context, t_element.context)
-            relations[((t_sent_id, t_indices), (f_sent_id, f_indices))] = (
-                relation, t_element.context, f_element.context)
+            indices_from = tuple(element_from.indices)
+            indices_to = tuple(element_to.indices)
 
-            for f_idx in f_indices:
-                relidxs[(f_sent_id, f_idx)] = (
-                f_indices, f_element.channel, f_element.ne)
-            for t_idx in t_indices:
-                relidxs[(t_sent_id, t_idx)] = (
-                t_indices, t_element.channel, t_element.ne)
+            # We consider relation data in both ways
+            relations[
+                ((sent_id_from, indices_from), (sent_id_to, indices_to))
+            ] = (relation, element_from.context, element_to.context)
+            relations[
+                ((sent_id_to, indices_to), (sent_id_from, indices_from))
+            ] = (relation, element_to.context, element_from.context)
+
+            # We add the same metadata to all indices in phrase
+            for idx_from in indices_from:
+                relidxs[(sent_id_from, idx_from)] = (
+                    indices_from, element_from.channel, element_from.ne)
+            for idx_to in indices_to:
+                relidxs[(sent_id_to, idx_to)] = (
+                    indices_to, element_to.channel, element_to.ne)
 
     for rel, rel_value in relations.items():
-        relation, f_context, t_context = rel_value
-        ((f_sent_id, f_indices), (t_sent_id, t_indices)) = rel
+        relation, context_from, context_to = rel_value
+        ((sent_id_from, indices_from), (sent_id_to, indices_to)) = rel
 
-        f_nouns = get_nouns_idx(sentences[f_sent_id])
-        t_nouns = get_nouns_idx(sentences[t_sent_id])
+        nouns_indices_from = get_nouns_idx(sentences[sent_id_from])
+        nouns_indices_to = get_nouns_idx(sentences[sent_id_to])
 
-        if f_sent_id == t_sent_id:
-            generator = permutations(f_nouns, 2)
+        if sent_id_from == sent_id_to:
+            nouns_indices_pairs = permutations(nouns_indices_from, 2)
         else:
-            generator = product(f_nouns, t_nouns)
+            nouns_indices_pairs = product(nouns_indices_from, nouns_indices_to)
 
-        for f_idx, t_idx in generator:
+        for idx_from, idx_to in nouns_indices_pairs:
             try:
-                _f_idxs, _f_channel_name, _f_ne = relidxs[(f_sent_id, f_idx)]
+                _f_idxs, _f_channel_name, _f_ne = relidxs[
+                    (sent_id_from, idx_from)]
             except KeyError:
                 _f_idxs = None
                 _f_channel_name = ''
-                _f_ne = 0.0
+                _f_ne = False
                 pass
 
             try:
-                _t_idxs, _t_channel_name, _t_ne = relidxs[(t_sent_id, t_idx)]
+                _t_idxs, _t_channel_name, _t_ne = relidxs[(sent_id_to, idx_to)]
             except KeyError:
                 _t_idxs = None
                 _t_channel_name = ''
-                _t_ne = 0.0
+                _t_ne = False
                 pass
 
-            if _t_idxs and _f_idxs and (
-            (t_sent_id, _t_idxs), (f_sent_id, _f_idxs)) in relations:
+            # If two nouns are part of phrases in relation
+            if _t_idxs \
+                    and _f_idxs \
+                    and ((sent_id_to, _t_idxs), (sent_id_from, _f_idxs)) \
+                    in relations:
                 continue
 
-            f_lemma = get_lemma(sentences[f_sent_id], f_idx)
-            t_lemma = get_lemma(sentences[t_sent_id], t_idx)
-            element_from = Relation.Element(f_sent_id, f_lemma,
-                                            _f_channel_name, _f_ne, [f_idx],
-                                            f_context)
-            element_to = Relation.Element(t_sent_id, t_lemma, _t_channel_name,
-                                          _t_ne, [t_idx], t_context)
+            f_lemma = get_lemma(sentences[sent_id_from], idx_from)
+            t_lemma = get_lemma(sentences[sent_id_to], idx_to)
+            element_from = Relation.Element(
+                sent_id_from, f_lemma, _f_channel_name,
+                _f_ne, [idx_from], context_from
+            )
+            element_to = Relation.Element(
+                sent_id_to, t_lemma, _t_channel_name,
+                _t_ne, [idx_to], context_to
+            )
 
-            id_dir = get_document_dir(document)
-            id_doc = get_document_file_name(document)
-            rel = Relation(id_doc, element_from, element_to)
-            lines.append(f'{id_dir}\tno_relation\t{rel}')
+            id_domain = get_document_dir(document)
+            id_document = get_document_file_name(document)
+            rel = Relation(id_document, element_from, element_to)
+
+            lines.append(f'no_relation\t{id_domain}\t{rel}')
+
     return lines
