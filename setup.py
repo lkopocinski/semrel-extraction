20106475ab75f59adffeb8b3bc70f72e47d868a3
lkopocinski <lkopocinski@gmail.com>
2020-01-14 15:07:54 +0100

    DVC remove test and dataset
    Former-commit-id: fc50544eb6859236a10b61e528dd659463b0f597

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='relextr',
-    version='0.1.1',
+    version='0.1.2',
     author='Arkadiusz Janz',
     author_email='arkadiusz.janz@pwr.edu.pl',
     maintainer='Łukasz Kopociński',
