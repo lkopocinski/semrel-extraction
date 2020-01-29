9d00cdea7798216a58659497f75b4239216a0af5
Łukasz Kopociński <lkopocinski@gmail.com>
2020-01-29 17:18:12 +0100

    Load map

--- data/2_vectorize.sh
+++ data/2_vectorize.sh
@@ -2,21 +2,20 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-DATA_IN="./data/relations"
+INPUT_PATH="./data/relations"
 OUTPUT_PATH="./data/vectors"
 SCRIPTS_DIR="./data/scripts"
 MAPS_DIR="./data/maps"
 
 dvc run \
--d ${DATA_IN} \
+-d ${INPUT_PATH} \
 -d ${MAPS_DIR} \
 -d ${SCRIPTS_DIR}/combine_vectors.py \
 -o ${OUTPUT_PATH} \
-
-${SCRIPTS_DIR}/combine_vectors.py --data-in "${DATA_IN}/relations." \
-                                 --output-path ${OUTPUT_PATH} \
-                                 --elmo-map "${MAPS_DIR}/elmo.map.pt" \
-                                 --fasttext-map "${MAPS_DIR}/fasttext.map.pt" \
-                                 --retrofit-map "${MAPS_DIR}/retrofit.map.pt"
+CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/combine_vectors.py --data-in "${INPUT_PATH}/relations.tsv" \
+                                                         --elmo-map "${MAPS_DIR}/elmo.map.pt" \
+                                                         --fasttext-map "${MAPS_DIR}/fasttext.map.pt" \
+                                                         --retrofit-map "${MAPS_DIR}/retrofit.map.pt" \
+                                                         --output-path ${OUTPUT_PATH}
 
 popd
