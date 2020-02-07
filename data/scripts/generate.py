da4506da0cdce633e3dde38ad94000716b0d242a
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-07 08:59:38 +0100

    Add pudb and ipython to requirements
    Former-commit-id: 3ceda680f19b13b0f42d80986b597bafcce90a5a

--- data/scripts/generate.py
+++ data/scripts/generate.py
@@ -7,17 +7,22 @@ from corpus import relations_documents_gen, relations_file_paths
 from data.scripts.generator import RelationsGenerator
 from data.scripts.utils.io import save_lines
 
-CHANNELS = (('BRAND_NAME', 'PRODUCT_NAME'), ('PRODUCT_NAME', 'BRAND_NAME'))
+CHANNELS = (('BRAND_NAME', 'PRODUCT_NAME'),
+            ('PRODUCT_NAME', 'BRAND_NAME'))
 
 
 @click.command()
 @click.option('--input-path', required=True, type=str,
               help='Path to corpora files.')
-@click.option('--directories', required=True, nargs=3,
+@click.option('--directories', required=True, nargs=1,
               help='Directories names with corpus files.')
 @click.option('--output-path', required=True, type=str,
               help='Directory for saving generated relations files.')
-def main(input_path, directories, output_path):
+def main(
+        input_path,
+        directories,
+        output_path
+):
     relations_files = relations_file_paths(input_path, directories)
     output_path = Path(output_path)
 
@@ -27,10 +32,11 @@ def main(input_path, directories, output_path):
         positive_relations = relations_generator.generate_positive(CHANNELS)
         positives_lines = [f'in_relation\t{document.directory}\t{relation}' for relation in positive_relations]
 
-        negative_relations = relations_generator.generate_negative(CHANNELS)
-        negatives_lines = [f'no_relation\t{document.directory}\t{relation}' for relation in negative_relations]
+        # negative_relations = relations_generator.generate_negative(CHANNELS)
+        # negatives_lines = [f'no_relation\t{document.directory}\t{relation}' for relation in negative_relations]
 
-        save_lines(output_path, positives_lines + negatives_lines, mode='a')
+        # save_lines(output_path, positives_lines + negatives_lines, mode='a')
+        save_lines(output_path, positives_lines, mode='a')
 
 
 if __name__ == "__main__":
