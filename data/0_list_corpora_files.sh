fe7dba00b30012d5f9b0e3f4c2501d5d861068c5
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-16 13:41:42 +0100

    List corpora files

---
+++ data/0_list_corpora_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -eux
+
+pushd "$(git rev-parse --show-toplevel)"
+
+CORPORA_DIR="./data/corpora/"
+OUTPUT_PATH="./data/relations_files.txt"
+
+dvc run \
+  -d ${CORPORA_DIR} \
+  -o ${OUTPUT_PATH} \
+  "find ${CORPORA_DIR} -type f -name "*.rel.xml" > ${OUTPUT_PATH}"
+
+popd
