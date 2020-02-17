183dff18785328102004031656c0d3132a37f80d
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-17 14:02:17 +0100

    Clean up 3_vectorize.sh

--- data/3_vectorize.sh
+++ data/3_vectorize.sh
@@ -2,20 +2,20 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-INPUT_PATH="./data/relations"
+INPUT_PATH="./data/relations/relations.tsv"
 OUTPUT_PATH="./data/vectors"
 SCRIPTS_DIR="./data/scripts"
 MAPS_DIR="./data/maps"
 
 dvc run \
--d ${INPUT_PATH} \
--d ${MAPS_DIR} \
--d ${SCRIPTS_DIR}/combine_vectors.py \
--o ${OUTPUT_PATH} \
-CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/combine_vectors.py --input-path "${INPUT_PATH}/relations.tsv" \
-                                                         --elmo-map "${MAPS_DIR}/elmo.map.pt" "${MAPS_DIR}/elmo.map.keys" \
-                                                         --fasttext-map "${MAPS_DIR}/fasttext.map.pt" "${MAPS_DIR}/fasttext.map.keys" \
-                                                         --retrofit-map "${MAPS_DIR}/retrofit.map.pt" "${MAPS_DIR}/retrofit.map.keys" \
-                                                         --output-path ${OUTPUT_PATH}
+  -d ${INPUT_PATH} \
+  -d ${SCRIPTS_DIR}/combine_vectors.py \
+  -d ${MAPS_DIR} \
+  -o ${OUTPUT_PATH} \
+  CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/combine_vectors.py --input-path ${INPUT_PATH} \
+                                                           --elmo-map "${MAPS_DIR}/elmo.map.pt" "${MAPS_DIR}/elmo.map.keys" \
+                                                           --fasttext-map "${MAPS_DIR}/fasttext.map.pt" "${MAPS_DIR}/fasttext.map.keys" \
+                                                           --retrofit-map "${MAPS_DIR}/retrofit.map.pt" "${MAPS_DIR}/retrofit.map.keys" \
+                                                           --output-path ${OUTPUT_PATH}
 
 popd
