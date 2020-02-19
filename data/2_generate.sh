e544994ee8427197fdef503a640c3f867ddc51b4
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-19 16:47:36 +0100

    Rename generate.py to generate_relations.py

--- data/2_generate.sh
+++ data/2_generate.sh
@@ -2,19 +2,20 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-INPUT_PATH="./data/relations_files.txt"
-OUTPUT_DIR="./data/relations"
-OUTPUT_PATH="${OUTPUT_DIR}/relations.tsv"
 SCRIPTS_DIR="./data/scripts"
+OUTPUT_DIR="./data/relations"
+
+INPUT_PATH="./data/relations_files.list"
+RELATIONS_PATH="${OUTPUT_DIR}/relations.tsv"
 
 mkdir -p ${OUTPUT_DIR}
 
 dvc run \
   -d ${INPUT_PATH} \
-  -d ${SCRIPTS_DIR}/generate.py \
+  -d ${SCRIPTS_DIR}/generate_relations.py \
   -d ${SCRIPTS_DIR}/generator.py \
-  -o ${OUTPUT_PATH} \
-  CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/generate.py --input-path ${INPUT_PATH} \
-                                                    --output-path ${OUTPUT_PATH}
+  -o ${RELATIONS_PATH} \
+  CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/generate_relations.py --input-path ${INPUT_PATH} \
+                                                              --output-path ${RELATIONS_PATH}
 
 popd
