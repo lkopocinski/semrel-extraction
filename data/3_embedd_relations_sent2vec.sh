32c6defb6f2e0d64209d743d39510ee6bea105e9
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-20 10:58:06 +0100

    Clean up 3_embedd_relations_sent2vec.sh

--- data/3_embedd_relations_sent2vec.sh
+++ data/3_embedd_relations_sent2vec.sh
@@ -3,11 +3,14 @@
 pushd "$(git rev-parse --show-toplevel)"
 
 RELATIONS_FILE="./data/relations/relations.tsv"
-DOCUMENTS_FILE="./data/relations_files.txt"
+DOCUMENTS_FILE="./data/relations_files.list"
 MODEL_DIR="./data/sent2vec"
 
-OUTPUT_DIR="./data/vectors"
 SCRIPTS_DIR="./data/scripts"
+OUTPUT_DIR='./dta/vectors'
+
+KEYS_FILE="${OUTPUT_DIR}/sent2vec.map.keys"
+VECTORS_FILE="${OUTPUT_DIR}/sent2vec.map.pt"
 
 mkdir -p ${OUTPUT_DIR}
 
@@ -15,10 +18,11 @@ dvc run \
   -d ${RELATIONS_FILE} \
   -d ${DOCUMENTS_FILE} \
   -d ${SCRIPTS_DIR}/make_sent2vec_map.py \
-  -o ${OUTPUT_DIR} \
+  -o ${KEYS_FILE} \
+  -o ${VECTORS_FILE} \
   CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/make_sent2vec_map.py --relations-file ${RELATIONS_FILE} \
                                                              --documents-files ${DOCUMENTS_FILE} \
                                                              --model "${MODEL_DIR}/kgr10.bin" \
-                                                             --output-paths "${OUTPUT_DIR}/sent2vec.map.keys" "${OUTPUT_DIR}/sent2vec.map.pt"
+                                                             --output-paths ${KEYS_FILE} ${VECTORS_FILE}
 
 popd
