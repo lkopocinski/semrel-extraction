41d84b356f0bab038d280b0f790f289f1c4dc1ba
Łukasz Kopociński <lkopocinski@gmail.com>
2020-02-24 14:23:45 +0100

    Remove shuffle

--- model/scripts/utils/data_loader.py
+++ model/scripts/utils/data_loader.py
@@ -178,13 +178,25 @@ def get_loaders(data_dir: str,
     train_indices, valid_indices, test_indices = ds_generator.generate_datasets(balanced, lexical_split, in_domain)
 
     train_loader = data.DataLoader(
-        dataset, batch_size, True, BaseSampler(train_indices), num_workers=num_workers, pin_memory=pin_memory,
+        dataset=dataset,
+        batch_size=batch_size,
+        sampler=BaseSampler(train_indices),
+        num_workers=num_workers,
+        pin_memory=pin_memory,
     )
     valid_loader = data.DataLoader(
-        dataset, batch_size, True, BaseSampler(valid_indices), num_workers=num_workers, pin_memory=pin_memory,
+        dataset=dataset,
+        batch_size=batch_size,
+        sampler=BaseSampler(valid_indices),
+        num_workers=num_workers,
+        pin_memory=pin_memory,
     )
     test_loader = data.DataLoader(
-        dataset, batch_size, True, BaseSampler(test_indices), num_workers=num_workers, pin_memory=pin_memory,
+        dataset=dataset,
+        batch_size=batch_size,
+        sampler=BaseSampler(test_indices),
+        num_workers=num_workers,
+        pin_memory=pin_memory,
     )
 
     return train_loader, valid_loader, test_loader, dataset.vector_size
