bf174d990ed5118dd2f1f691cb32d7c6bb85e4ff
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-02 12:30:50 +0100

    Generate dataset indices

--- data/spert/generate_indices.py
+++ data/spert/generate_indices.py
@@ -1,5 +1,6 @@
+#!/usr/bin/env python3.6
+
 from pathlib import Path
-from typing import List
 
 import click
 
@@ -8,38 +9,30 @@ from io import save_json
 from runs import RUNS
 
 
-def get_indices(data_dir: str,
-                keys_file: str,
-                vectors_files: List[str],
+def get_indices(keys_file: str,
                 balanced: bool = False,
                 lexical_split: bool = False,
                 in_domain: str = None,
                 random_seed: int = 42):
-    keys_path = f'{data_dir}/{keys_file}'
-    vectors_files = [f'{data_dir}/{file}' for file in vectors_files]
-
-    dataset = BrandProductDataset(keys_file=keys_path, vectors_files=vectors_files)
-    ds_generator = DatasetGenerator(dataset, random_seed)
+    keys_path = Path(keys_file)
+    keys = BrandProductDataset._load_keys(keys_path)
+    ds_generator = DatasetGenerator(keys, random_seed)
     return ds_generator.generate_datasets(balanced, lexical_split, in_domain)
 
 
 @click.command()
-@click.option('--data-dir', required=True, type=str)
-@click.option('--data-keys', required=True, type=str)
-@click.option('--runs-name', required=True, type=str)
-def main(data_dir, data_keys, runs_name):
+@click.option('--dataset-keys', required=True, type=str)
+@click.option('--output-path', required=True, type=str)
+def main(data_keys, output_path):
     indices = {}
 
-    runs = RUNS[runs_name]
-    for index, params in runs.items():
+    default_runs = RUNS['default']
+    for index, params in default_runs.items():
         in_domain = params.get('in_domain')
         lexical_split = params.get('lexical_split', False)
-        methods = params.get('methods', [])
 
         train, valid, test = get_indices(
-            data_dir=data_dir,
             keys_file=data_keys,
-            vectors_files=[f'{method}.rel.pt' for method in methods],
             balanced=True,
             lexical_split=lexical_split,
             in_domain=in_domain,
@@ -51,7 +44,7 @@ def main(data_dir, data_keys, runs_name):
             'test': test
         }
 
-        save_json(indices, Path(f'./runs_indices.json'))
+        save_json(indices, Path(output_path))
 
 
 if __name__ == '__main__':
