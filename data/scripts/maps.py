e55b274e37dd4529a8e86b2c68c1b6462ff75be6
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-16 13:12:17 +0100

    Refactor maps.py

--- data/scripts/maps.py
+++ data/scripts/maps.py
@@ -1,50 +1,17 @@
+from itertools import chain
 from pathlib import Path
-from typing import Iterator, List
+from typing import Iterator, List, NamedTuple, Dict, Tuple
 
 import torch
 
+from data.scripts.keys import make_sentence_keys
 from data.scripts.utils.corpus import DocSentence, Document
 from data.scripts.utils.vectorizers import Vectorizer
 
 
-class MapMaker:
-
-    def __init__(self, vectorizer: Vectorizer):
-        self._vectorizer = vectorizer
-
-    @staticmethod
-    def make_keys(document: Document, sentence: DocSentence) -> List[str]:
-        return [f'{document.directory}\t{document.id}\t{sentence.id}\t{id_token}'
-                for id_token, _ in enumerate(sentence.orths)]
-
-    def make_sentence_map(self, document: Document, sentence: DocSentence) -> [List[tuple], torch.Tensor]:
-        keys = self.make_keys(document, sentence)
-        vectors = self._vectorizer.embed(sentence.orths)
-        return keys, vectors
-
-    def make_document_map(self, document: Document) -> [List[tuple], torch.Tensor]:
-        keys = []
-        vectors = []
-
-        for sentence in document.sentences:
-            sentence_keys, sentence_vectors = self.make_sentence_map(document, sentence)
-            keys.extend(sentence_keys)
-            vectors.extend(sentence_vectors)
-
-        return keys, vectors
-
-    def make_map(self, documents: Iterator[Document]) -> [List[tuple], torch.Tensor]:
-        keys = []
-        vectors = []
-
-        for idx, document in enumerate(documents):
-            if idx % 400 == 0:
-                print(f"{idx} / 12040")
-            document_keys, document_tensor = self.make_document_map(document)
-            keys.extend(document_keys)
-            vectors.extend(document_tensor)
-
-        return keys, torch.stack(vectors)
+class VectorsMap(NamedTuple):
+    keys: Dict[Tuple]
+    vectors: torch.Tensor
 
 
 class MapLoader:
@@ -53,7 +20,7 @@ class MapLoader:
         self._keys_path = Path(keys_file)
         self._vectors_file = Path(vectors_file)
 
-    def _load_keys(self) -> dict:
+    def _load_keys(self) -> Dict[Tuple]:
         with self._keys_path.open('r', encoding='utf-8') as file:
             return {
                 tuple(line.strip().split('\t')): index
@@ -63,7 +30,34 @@ class MapLoader:
     def _load_vectors(self) -> torch.Tensor:
         return torch.load(str(self._vectors_file))
 
-    def __call__(self) -> [dict, torch.Tensor]:
+    def __call__(self) -> VectorsMap:
         keys = self._load_keys()
         vectors = self._load_vectors()
+        return VectorsMap(keys, vectors)
+
+
+class MapMaker:
+
+    def __init__(self, vectorizer: Vectorizer):
+        self._vectorizer = vectorizer
+
+    def make_sentence_map(self, document: Document, sentence: DocSentence) -> [List[str], torch.Tensor]:
+        keys = make_sentence_keys(document, sentence)
+        vectors = self._vectorizer.embed(sentence.orths)
+        return keys, vectors
+
+    def make_document_map(self, document: Document) -> [List[str], torch.Tensor]:
+        sentences_maps = [self.make_sentence_map(document, sentence) for sentence in document.sentences]
+        keys, vectors = zip(*sentences_maps)
+
+        keys = list(chain(*keys))
+        vectors = torch.stack(vectors)
+        return keys, vectors
+
+    def make_map(self, documents: Iterator[Document]) -> [List[str], torch.Tensor]:
+        document_maps = [self.make_document_map(document) for document in documents]
+        keys, vectors = zip(*document_maps)
+
+        keys = list(chain(*keys))
+        vectors = torch.stack(vectors)
         return keys, vectors
