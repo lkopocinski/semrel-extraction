bf5b89dbee3b96722365f0cfddb8260b050822b8
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-18 13:12:30 +0100

    Fix replace rel with ner issue

--- semrel/data/scripts/corpus/utils.py
+++ semrel/data/scripts/corpus/utils.py
@@ -9,13 +9,14 @@ from semrel.data.scripts.corpus import Document
 
 def from_index_documents_gen(relations_files_index: Path) -> Iterator[Document]:
     with relations_files_index.open('r', encoding='utf-8') as file:
-        relation_files = [Path(line.strip()) for line in file]
-        return documents_gen(relation_files)
+        relations_files = [Path(line.strip()) for line in file]
+        return documents_gen(relations_files)
 
 
 def documents_gen(relations_files: Iterator[Path]) -> Iterator[Document]:
     for rel_path in relations_files:
-        ccl_path = Path(str(rel_path).replace('rel', 'ner'))
+        ccl_name = rel_path.name.replace('.rel', '.ner')
+        ccl_path = rel_path.parent / ccl_name
         if rel_path.is_file() and ccl_path.is_file():
             ccl_document = ccl.read_ccl_and_rel_ccl(
                 ccl_file=str(ccl_path), rel_ccl_file=str(rel_path)
