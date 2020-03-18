bbca35140aed6b5bbeaba43167400775018c8829
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-18 12:17:11 +0100

    Change paths

--- semrel/data/1_make_elmo_map.sh
+++ semrel/data/1_make_elmo_map.sh
@@ -2,24 +2,28 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-MODEL_DIR="./semrel/data/data/elmo"
-SCRIPTS_DIR="./semrel/data/scripts/cli"
+ROOT_DIR="./semrel/data"
+DATA_DIR="${ROOT_DIR}/data"
 
-INPUT_PATH="./semrel/data/data/relations.files.list"
-KEYS_FILE="./semrel/data/data/maps/elmo.map.keys"
-VECTORS_FILE="./semrel/data/data/maps/elmo.map.pt"
+MODEL_DIR="${DATA_DIR}/elmo"
+SCRIPTS_DIR="${ROOT_DIR}/scripts"
+CLI_DIR="${SCRIPTS_DIR}/cli"
 
-mkdir -p "./semrel/data/data/maps/"
+INPUT_PATH="${DATA_DIR}/relations.files.list"
+KEYS_FILE="${DATA_DIR}/maps/elmo.map.keys"
+VECTORS_FILE="${DATA_DIR}/maps/elmo.map.pt"
+
+mkdir -p "${DATA_DIR}/maps/"
 
 dvc run \
   -d ${INPUT_PATH} \
-  -d ${SCRIPTS_DIR}/make_elmo_map.py \
+  -d ${CLI_DIR}/make_elmo_map.py \
   -d ${SCRIPTS_DIR}/maps.py \
   -o ${KEYS_FILE} \
   -o ${VECTORS_FILE} \
-  -f elmo.map.dvc \
-  CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/make_elmo_map.py --input-path ${INPUT_PATH} \
-                                                         --model "${MODEL_DIR}/options.json" "${MODEL_DIR}/weights.hdf5" \
-                                                         --output-paths ${KEYS_FILE} ${VECTORS_FILE}
+  -f _elmo.map.dvc \
+  CUDA_VISIBLE_DEVICES=0 ${CLI_DIR}/make_elmo_map.py --input-path ${INPUT_PATH} \
+                                                     --model "${MODEL_DIR}/options.json" "${MODEL_DIR}/weights.hdf5" \
+                                                     --output-paths ${KEYS_FILE} ${VECTORS_FILE}
 
 popd
