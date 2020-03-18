64cef036af345e4a99d67bc847772ce1d80b47f8
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-18 14:41:18 +0100

    Adapt paths

--- semrel/data/3_embedd_relations_ner.sh
+++ semrel/data/3_embedd_relations_ner.sh
@@ -2,10 +2,14 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-RELATIONS_FILE="./semrel/data/data/relations/relations.tsv"
+ROOT_DIR="./semrel/data"
+DATA_DIR="${ROOT_DIR}/data"
 
-SCRIPTS_DIR="./semrel/data/data/scripts/cli"
-OUTPUT_DIR="./semrel/data/data/vectors"
+RELATIONS_FILE="${DATA_DIR}/relations/relations.tsv"
+
+SCRIPTS_DIR="${ROOT_DIR}/scripts"
+CLI_DIR="${SCRIPTS_DIR}/cli"
+OUTPUT_DIR="${DATA_DIR}/vectors"
 
 KEYS_FILE="${OUTPUT_DIR}/ner.rel.keys"
 VECTORS_FILE="${OUTPUT_DIR}/ner.rel.pt"
@@ -14,11 +18,11 @@ mkdir -p ${OUTPUT_DIR}
 
 dvc run \
   -d ${RELATIONS_FILE} \
-  -d ${SCRIPTS_DIR}/make_ner_map.py \
+  -d ${CLI_DIR}/make_ner_map.py \
   -o ${KEYS_FILE} \
   -o ${VECTORS_FILE} \
-  -f vectors.ner.dvc \
-  ${SCRIPTS_DIR}/make_ner_map.py --relations-file ${RELATIONS_FILE} \
-                                 --output-paths ${KEYS_FILE} ${VECTORS_FILE}
+  -f _vectors.ner.dvc \
+  ${CLI_DIR}/make_ner_map.py --relations-file ${RELATIONS_FILE} \
+                             --output-paths ${KEYS_FILE} ${VECTORS_FILE}
 
 popd
--- semrel/data/3_embedd_relations_sent2vec.sh
+++ semrel/data/3_embedd_relations_sent2vec.sh
@@ -2,12 +2,16 @@
 
 pushd "$(git rev-parse --show-toplevel)"
 
-RELATIONS_FILE="./semrel/data/data/relations/relations.tsv"
-DOCUMENTS_FILE="./semrel/data/data/relations.files.list"
-MODEL_DIR="./semrel/data/data/sent2vec"
+ROOT_DIR="./semrel/data"
+DATA_DIR="${ROOT_DIR}/data"
 
-SCRIPTS_DIR="./semrel/data/data/scripts/cli"
-OUTPUT_DIR='./semrel/data/data/vectors'
+RELATIONS_FILE="${DATA_DIR}/relations/relations.tsv"
+DOCUMENTS_FILE="${DATA_DIR}/relations.files.list"
+MODEL_DIR="${DATA_DIR}/sent2vec"
+
+SCRIPTS_DIR="${ROOT_DIR}/scripts"
+CLI_DIR="${SCRIPTS_DIR}/cli"
+OUTPUT_DIR="${DATA_DIR}/vectors"
 
 KEYS_FILE="${OUTPUT_DIR}/sent2vec.rel.keys"
 VECTORS_FILE="${OUTPUT_DIR}/sent2vec.rel.pt"
@@ -17,13 +21,13 @@ mkdir -p ${OUTPUT_DIR}
 dvc run \
   -d ${RELATIONS_FILE} \
   -d ${DOCUMENTS_FILE} \
-  -d ${SCRIPTS_DIR}/make_sent2vec_map.py \
+  -d ${CLI_DIR}/make_sent2vec_map.py \
   -o ${KEYS_FILE} \
   -o ${VECTORS_FILE} \
-  -f vectors.sent2vec.dvc \
-  CUDA_VISIBLE_DEVICES=0 ${SCRIPTS_DIR}/make_sent2vec_map.py --relations-file ${RELATIONS_FILE} \
-                                                             --documents-files ${DOCUMENTS_FILE} \
-                                                             --model "${MODEL_DIR}/kgr10.bin" \
-                                                             --output-paths ${KEYS_FILE} ${VECTORS_FILE}
+  -f _vectors.sent2vec.dvc \
+  CUDA_VISIBLE_DEVICES=0 ${CLI_DIR}/make_sent2vec_map.py --relations-file ${RELATIONS_FILE} \
+                                                         --documents-files ${DOCUMENTS_FILE} \
+                                                         --model "${MODEL_DIR}/kgr10.bin" \
+                                                         --output-paths ${KEYS_FILE} ${VECTORS_FILE}
 
 popd
