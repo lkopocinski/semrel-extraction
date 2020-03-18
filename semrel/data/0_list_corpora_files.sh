fbca49132858906dc1174cbad8a63cdb84569a91
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-18 12:05:43 +0100

    Generate relations files list

--- semrel/data/0_list_corpora_files.sh
+++ semrel/data/0_list_corpora_files.sh
@@ -8,6 +8,7 @@ OUTPUT_FILE="./semrel/data/data/relations.files.list"
 dvc run \
   -d ${CORPORA_DIR} \
   -o ${OUTPUT_FILE} \
+  -f _relations.files.list.dvc \
   "find ${CORPORA_DIR} -type f -name "*.rel.xml" > ${OUTPUT_FILE}"
 
 popd
