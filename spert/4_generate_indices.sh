1710d91be81e275e8cf554a968a806b49f59cf2d
Łukasz Kopociński <lkopocinski@gmail.com>
2020-03-24 16:16:31 +0100

    Generate spert jsons

--- spert/4_generate_indices.sh
+++ spert/4_generate_indices.sh
@@ -3,7 +3,7 @@
 pushd "$(git rev-parse --show-toplevel)"
 
 DATA_DIR="./semrel/data/data"
-VECTORS_DIR="${DATA_DIR}/vectors"
+KEYS_PATH="${DATA_DIR}/vectors/elmo.rel.keys"
 
 SCRIPTS_DIR="./spert/scripts"
 OUTPUT_DIR="./spert/data"
@@ -12,11 +12,11 @@ OUTPUT_PATH="${OUTPUT_DIR}/indices.json"
 mkdir -p ${OUTPUT_DIR}
 
 dvc run \
-  -d ${VECTORS_DIR} \
+  -d ${KEYS_PATH} \
   -d ${SCRIPTS_DIR}/generate_indices.py \
   -o ${OUTPUT_PATH} \
   -f _spert.indices.dvc \
-  ${SCRIPTS_DIR}/generate_indices.py --dataset-keys ${VECTORS_DIR}/elmo.rel.keys \
+  ${SCRIPTS_DIR}/generate_indices.py --dataset-keys ${KEYS_PATH} \
                                      --output-path ${OUTPUT_PATH}
 
 popd
